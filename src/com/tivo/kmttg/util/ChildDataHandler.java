21ce786395ffb78cfa1995c4c5cb6d31607f9e7e
moyekj@yahoo.com <moyekj@yahoo.com@b0df3fdd-2057-4f73-b226-dfc9190e8f7a>
2009-05-22 20:51:42 +0000

    Open streams in constructor instead of run() otherwise some stdout/stderr was being lost
    git-svn-id: https://svn.code.sf.net/p/kmttg/code/trunk@125 b0df3fdd-2057-4f73-b226-dfc9190e8f7a

--- src/com/tivo/kmttg/util/ChildDataHandler.java
+++ src/com/tivo/kmttg/util/ChildDataHandler.java
@@ -1,6 +1,7 @@
 package com.tivo.kmttg.util;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Stack;
@@ -8,18 +9,21 @@ import java.util.Stack;
 
 public class ChildDataHandler extends Thread {
    InputStream inputStream;
+   InputStreamReader inputStreamReader;
+   BufferedReader bufferedReader;
+   
    public Stack<String> output;
    
    ChildDataHandler(InputStream inputStream, Stack<String> output) {
      this.inputStream = inputStream;
      this.output = output;
+     inputStreamReader = new InputStreamReader(inputStream);
+     bufferedReader = new BufferedReader(inputStreamReader);
    }
    
    public void run() {
      debug.print("");
      try {
-       InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-       BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
        String line=null;
        while((line = bufferedReader.readLine()) != null) {
          output.add(line);
@@ -27,5 +31,16 @@ public class ChildDataHandler extends Thread {
      } catch(Exception e) {
         output.add(e.getMessage());
      }
+   }
+   
+   public void close() {
+      if (inputStream != null) {
+         run();
+         try {
+            inputStream.close();
+         } catch (IOException e) {
+            return;
+         }
+      }
    }
 }
