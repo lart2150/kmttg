4d968bb5e5f197d3f4f51b0a9e2e4a9b89a841e5
moyekj@yahoo.com <moyekj@yahoo.com@b0df3fdd-2057-4f73-b226-dfc9190e8f7a>
2009-05-27 22:27:53 +0000

    Initial version
    git-svn-id: https://svn.code.sf.net/p/kmttg/code/trunk@190 b0df3fdd-2057-4f73-b226-dfc9190e8f7a

---
+++ src/com/tivo/kmttg/task/adscan.java
@@ -0,0 +1,158 @@
+package com.tivo.kmttg.task;
+
+import java.io.File;
+import java.util.Date;
+import java.util.Stack;
+
+import com.tivo.kmttg.main.config;
+import com.tivo.kmttg.main.jobData;
+import com.tivo.kmttg.main.jobMonitor;
+import com.tivo.kmttg.util.backgroundProcess;
+import com.tivo.kmttg.util.debug;
+import com.tivo.kmttg.util.file;
+import com.tivo.kmttg.util.log;
+
+public class adscan {
+   String  vrdscript = null;
+   String  cscript = null;
+   private backgroundProcess process;
+   private jobData job;
+
+   // constructor
+   public adscan(jobData job) {
+      debug.print("job=" + job);
+      this.job = job;
+   }
+   
+   public backgroundProcess getProcess() {
+      return process;
+   }
+   
+   public Boolean launchJob() {
+      debug.print("");
+      Boolean schedule = true;
+      // Don't adscan if vprjFile already exists
+      if ( file.isFile(job.vprjFile) ) {
+         log.warn("SKIPPING ADSCAN, FILE ALREADY EXISTS: " + job.vprjFile);
+         schedule = false;
+      }
+      
+      String s = File.separator;
+      vrdscript = config.VRD + s + "AdScan.vbs";
+      cscript = System.getenv("SystemRoot") + s + "system32" + s + "cscript.exe";
+      
+      if ( ! file.isFile(vrdscript) ) {
+         log.error("File does not exist: " + vrdscript);
+         schedule = false;
+      }
+      
+      if ( ! file.isFile(cscript) ) {
+         log.error("File does not exist: " + cscript);
+         schedule = false;
+      }
+                  
+      if ( ! file.isFile(job.mpegFile) ) {
+         log.error("mpeg file not found: " + job.mpegFile);
+         schedule = false;
+      }
+      
+      if (schedule) {
+         // Create sub-folders for output file if needed
+         if ( ! jobMonitor.createSubFolders(job.vprjFile) ) schedule = false;
+      }
+      
+      if (schedule) {
+         if ( start() ) {
+            job.process_adscan   = this;
+            job.status           = "running";
+            job.time             = new Date().getTime();
+         }
+         return true;
+      } else {
+         return false;
+      }      
+   }
+
+   // Return false if starting command fails, true otherwise
+   private Boolean start() {
+      debug.print("");
+      Stack<String> command = new Stack<String>();
+      command.add(cscript);
+      command.add("//nologo");
+      command.add(vrdscript);
+      command.add(job.mpegFile);
+      command.add(job.vprjFile);
+      command.add("/q");
+      process = new backgroundProcess();
+      log.print(">> Running adscan on " + job.mpegFile + " ...");
+      if ( process.run(command) ) {
+         log.print(process.toString());
+      } else {
+         log.error("Failed to start command: " + process.toString());
+         process.printStderr();
+         process = null;
+         jobMonitor.removeFromJobList(job);
+         return false;
+      }
+      return true;
+   }
+   
+   public void kill() {
+      debug.print("");
+      process.kill();
+      log.warn("Killing '" + job.type + "' job: " + process.toString());
+   }
+
+   // Check status of a currently running job
+   // Returns true if still running, false if job finished
+   // If job is finished then check result
+   public Boolean check() {
+      //debug.print("");
+      int exit_code = process.exitStatus();
+      if (exit_code == -1) {
+         // Still running
+         if (config.GUI) {
+            // Update STATUS column
+            if ( file.isFile(job.mpegFile_cut) ) {               
+               // Update status in job table
+               String t = jobMonitor.getElapsedTime(job.time);
+               config.gui.jobTab_UpdateJobMonitorRowStatus(job, t);
+               
+               if ( jobMonitor.isFirstJobInMonitor(job) ) {
+                  // If 1st job then update title & progress bar
+                  String title = String.format("adscan: %s %s", t, config.kmttg);
+                  config.gui.setTitle(title);
+               }
+            }
+         }
+        return true;
+      } else {
+         // Job finished         
+         if ( jobMonitor.isFirstJobInMonitor(job) ) {
+            config.gui.setTitle(config.kmttg);
+         }
+         jobMonitor.removeFromJobList(job);
+         
+         // Check for problems
+         int failed = 0;
+         // No or empty vprjFile means failure
+         if ( ! file.isFile(job.vprjFile) || file.isEmpty(job.vprjFile) ) {
+            failed = 1;
+         }
+         
+         // exit status != 0 => problem
+         if (exit_code != 0) {
+            failed = 1;
+         }
+         
+         if (failed == 1) {
+            log.error("adscan failed (exit code: " + exit_code + " ) - check command: " + process.toString());
+            process.printStderr();
+         } else {
+            log.warn("adscan job completed: " + jobMonitor.getElapsedTime(job.time));
+            log.print("---DONE---");
+         }
+      }
+      return false;
+   }
+}
