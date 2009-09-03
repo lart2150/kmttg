afb481e7492a59bbbd7381551aebe19bd2a4d41f
moyekj@yahoo.com <moyekj@yahoo.com@b0df3fdd-2057-4f73-b226-dfc9190e8f7a>
2009-09-03 02:01:36 +0000

    Allow metadata syntax without spaces between name:value & take out quotes of parsed metadata values
    git-svn-id: https://svn.code.sf.net/p/kmttg/code/trunk@489 b0df3fdd-2057-4f73-b226-dfc9190e8f7a

--- src/com/tivo/kmttg/task/atomic.java
+++ src/com/tivo/kmttg/task/atomic.java
@@ -155,8 +155,10 @@ public class atomic {
          String name="", value="";
          while ( (line = ifp.readLine()) != null ) {
             debug.print("line=" + line);
-            name  = line.replaceFirst("(.+)\\s+:\\s+(.+)$", "$1");
-            value = line.replaceFirst("(.+)\\s+:\\s+(.+)$", "$2");
+            name  = line.replaceFirst("(.+)\\s*:\\s*(.+)$", "$1");
+            value = line.replaceFirst("(.+)\\s*:\\s*(.+)$", "$2");
+            // Get rid of quotes in value
+            value = line.replaceAll("\"", "");
             if ( ! h.containsKey(name) ) h.put(name, value);
          }
          ifp.close();
