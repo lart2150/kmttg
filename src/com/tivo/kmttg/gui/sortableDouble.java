22d622d1635caa45d7b9b16a35f748344709f5af
moyekj@yahoo.com <moyekj@yahoo.com@b0df3fdd-2057-4f73-b226-dfc9190e8f7a>
2009-10-09 21:33:40 +0000

    New data type for bitrateTable use
    git-svn-id: https://svn.code.sf.net/p/kmttg/code/trunk@585 b0df3fdd-2057-4f73-b226-dfc9190e8f7a

---
+++ src/com/tivo/kmttg/gui/sortableDouble.java
@@ -0,0 +1,17 @@
+package com.tivo.kmttg.gui;
+
+public class sortableDouble {
+   String display;
+   Double sortable;
+   
+   // Single entry constructor
+   sortableDouble(Double num) {
+      display = String.format("%.2f", num);
+      sortable = num;
+   }
+      
+   public String toString() {
+      return display;
+   }
+
+}
