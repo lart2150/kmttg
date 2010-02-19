7c78ee29bdc536d98d0ab011f73dede0bba0a07a
moyekj@yahoo.com <moyekj@yahoo.com@b0df3fdd-2057-4f73-b226-dfc9190e8f7a>
2010-02-19 23:04:09 +0000

    Added url to tivodecode & qsfix tasks in case of WebPlus delete needed
    git-svn-id: https://svn.code.sf.net/p/kmttg/code/trunk@966 b0df3fdd-2057-4f73-b226-dfc9190e8f7a

--- src/com/tivo/kmttg/main/jobMonitor.java
+++ src/com/tivo/kmttg/main/jobMonitor.java
@@ -791,6 +791,7 @@ public class jobMonitor {
          job.name         = config.tivodecode;
          job.tivoFile     = tivoFile;
          job.mpegFile     = mpegFile;
+         job.url          = entry.get("url"); // This added in case TivoWebPlus delete needed
          submitNewJob(job);
       }
       
@@ -804,6 +805,7 @@ public class jobMonitor {
          job.mpegFile_fix = mpegFile_fix;
          if (config.VrdDecrypt == 1) {
             job.tivoFile  = tivoFile;
+            job.url       = entry.get("url"); // This added in case TivoWebPlus delete needed
          }
          submitNewJob(job);
       }
