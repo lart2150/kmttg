5ca08b9f4dc8c1901afbbd4d61a762da52570bc5
moyekj@yahoo.com <moyekj@yahoo.com@b0df3fdd-2057-4f73-b226-dfc9190e8f7a>
2010-03-20 19:56:48 +0000

    Added twpdelete option
    git-svn-id: https://svn.code.sf.net/p/kmttg/code/trunk@993 b0df3fdd-2057-4f73-b226-dfc9190e8f7a

--- src/com/tivo/kmttg/main/autoEntry.java
+++ src/com/tivo/kmttg/main/autoEntry.java
@@ -9,16 +9,17 @@ public class autoEntry {
    
    // options
    public String tivo  = "all";
-   public int metadata = 0;
-   public int decrypt  = 0;
-   public int qsfix    = 0;
-   public int comskip  = 0;
-   public int comcut   = 0;
-   public int captions = 0;
-   public int encode   = 0;
-   public int push     = 0;
-   public int custom   = 0;
-   public int enabled  = 1;
+   public int metadata  = 0;
+   public int decrypt   = 0;
+   public int qsfix     = 0;
+   public int twpdelete = 0;
+   public int comskip   = 0;
+   public int comcut    = 0;
+   public int captions  = 0;
+   public int encode    = 0;
+   public int push      = 0;
+   public int custom    = 0;
+   public int enabled   = 1;
    public String encode_name = null;
    public String comskipIni = "none";
    public int suggestionsFilter = 0;
