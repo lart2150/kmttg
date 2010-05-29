c19706f627eefc714a254c6c469ba7c5b7dc6e68
moyekj@yahoo.com <moyekj@yahoo.com@b0df3fdd-2057-4f73-b226-dfc9190e8f7a>
2010-05-29 19:57:03 +0000

    Added new video files filter for common video file extensions
    git-svn-id: https://svn.code.sf.net/p/kmttg/code/trunk@1090 b0df3fdd-2057-4f73-b226-dfc9190e8f7a

--- src/com/tivo/kmttg/gui/fileBrowser.java
+++ src/com/tivo/kmttg/gui/fileBrowser.java
@@ -14,12 +14,12 @@ public class fileBrowser {
       Browser = new JFileChooser(config.TIVOS.get("FILES"));
       Browser.setFileSelectionMode(JFileChooser.FILES_ONLY);
       Browser.setMultiSelectionEnabled(true);
-      Browser.addChoosableFileFilter(new TextFilter());
+      Browser.addChoosableFileFilter(new videoFilter());
+      Browser.addChoosableFileFilter(new TivoFilter());
       //Browser.setAcceptAllFileFilterUsed(false);
    }
    
-   class TextFilter extends FileFilter {
-
+   class TivoFilter extends FileFilter {
       public boolean accept(File f) {
         if (f.isDirectory())
           return true;
@@ -32,6 +32,28 @@ public class fileBrowser {
 
       public String getDescription() {
         return "tivo and mpg";
+      }
+    }
+   
+   class videoFilter extends FileFilter {
+      public boolean accept(File f) {
+        if (f.isDirectory())
+          return true;
+        String s = f.getName().toLowerCase();
+        String[] valid = new String[] {
+           ".tivo", ".mpeg", ".mpg", ".asf", ".avi", ".dvr-ms", ".flv",
+           ".m2p", ".m4v", ".mkv", ".mov", ".mp4", ".mpeg4", ".m2ts", ".mts",
+           ".ogm", ".tp", ".ts", ".vob", ".wmv", ".wtv", ".xvid", ".divx", ".dvx"
+        };
+        for (int i=0; i<valid.length; ++i)
+        if ( s.endsWith(valid[i])) {
+           return true;
+        }
+        return false;
+      }
+
+      public String getDescription() {
+        return "video files";
       }
     }
 
