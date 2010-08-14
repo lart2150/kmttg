fa14b506d7bc21aac55ea8a5d6c5fafb0da614c8
moyekj@yahoo.com <moyekj@yahoo.com@b0df3fdd-2057-4f73-b226-dfc9190e8f7a>
2010-08-14 17:10:33 +0000

    Don't show 0 size file in job monitor, simplify title & status display in GUI mode
    git-svn-id: https://svn.code.sf.net/p/kmttg/code/trunk@1128 b0df3fdd-2057-4f73-b226-dfc9190e8f7a

--- src/com/tivo/kmttg/task/encode.java
+++ src/com/tivo/kmttg/task/encode.java
@@ -3,6 +3,8 @@ package com.tivo.kmttg.task;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.Stack;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.tivo.kmttg.main.config;
 import com.tivo.kmttg.main.encodeConfig;
@@ -130,71 +132,68 @@ public class encode implements Serializable {
       if (exit_code == -1) {
          // Still running
          if (config.GUI) {
+            String t = jobMonitor.getElapsedTime(job.time);
+            String size = null;
             if ( file.isFile(job.encodeFile) ) {               
-               String s = String.format("%.2f MB", (float)file.size(job.encodeFile)/Math.pow(2,20));
-               String t = jobMonitor.getElapsedTime(job.time);
+               size = String.format("%.2f MB", (float)file.size(job.encodeFile)/Math.pow(2,20));
+               if (size.equals("0.00 MB")) size = null;
+            }
                
-               // Try and determine pct complete
-               int pct = -1;
-               // For ffmpeg job get show duration for computing pct complete
-               if (process.toString().contains("ffmpeg")) {
-                  // Get duration from ffmpeg stderr if not yet available
-                  if ( job.duration == null ) {
-                     long duration = ffmpegGetDuration();
-                     if ( duration > 0 ) {
-                        job.duration = duration;
-                     }
-                  }
-                  // Get time from ffmpeg stderr
-                  long time = ffmpegGetTime();
-                  if (job.duration != null && time > 0) {
-                     Long duration = (Long)job.duration;
-                     pct = Integer.parseInt(String.format("%d", time*100/duration));
+            // Try and determine pct complete
+            int pct = -1;
+            
+            if (process.toString().contains(config.handbrake)) {
+               // Get pct complete from handbrake stdout
+               pct = handbrakeGetPct();
+            }
+            
+            else if (process.toString().contains(config.mencoder)) {
+               // Get pct complete from mencoder stdout
+               pct = mencoderGetPct();
+            }
+            
+            else if (process.toString().contains("ffmpeg")) {
+               // Get duration from ffmpeg stderr if not yet available
+               if ( job.duration == null ) {
+                  long duration = ffmpegGetDuration();
+                  if ( duration > 0 ) {
+                     job.duration = duration;
                   }
                }
-               
-               else if (process.toString().contains(config.handbrake)) {
-                  // Get pct complete from handbrake stdout
-                  pct = handbrakeGetPct();
-               }
-               
-               else {
-                  // Some other encoder
-                  pct = -2;
-               }
-                              
-               if ( jobMonitor.isFirstJobInMonitor(job) ) {
-                  // Update STATUS column
-                  config.gui.jobTab_UpdateJobMonitorRowStatus(job, t + "---" + s);
-                  
-                  // If 1st job then update title with pct complete
-                  if (pct > -1) {
-                     String title = String.format("encode: %d%% %s", pct, config.kmttg);
-                     config.gui.setTitle(title);
-                     config.gui.progressBar_setValue(pct);                  
-                  }
-                  if (pct == -2) {
-                     String title = String.format("encode: %s %s", t, config.kmttg);
-                     config.gui.setTitle(title);                     
-                  }
-               } else {
-                  // Update STATUS column
-                  if (pct > -1)
-                     config.gui.jobTab_UpdateJobMonitorRowStatus(job, String.format("%d%%",pct) + "---" + s);
-                  else
-                     config.gui.jobTab_UpdateJobMonitorRowStatus(job, t + "---" + s);                     
-               }
-            } else {
-               // File not yet available so simply update time
-               String t = jobMonitor.getElapsedTime(job.time);
-               config.gui.jobTab_UpdateJobMonitorRowStatus(job, t);
-               if ( jobMonitor.isFirstJobInMonitor(job) ) {
-                  String title = String.format("encode: %s %s", t, config.kmttg);
-                  config.gui.setTitle(title);
+               // Get time from ffmpeg stderr
+               long time = ffmpegGetTime();
+               if (job.duration != null && time > 0) {
+                  Long duration = (Long)job.duration;
+                  pct = Integer.parseInt(String.format("%d", time*100/duration));
                }
             }
+            
+            else {
+               // Some other encoder
+               pct = -2;
+            }
+
+            String status = t;
+            if ( jobMonitor.isFirstJobInMonitor(job) ) {
+               // If 1st job then update title with pct complete
+               if (pct > -1) {
+                  String title = String.format("encode: %d%% %s", pct, config.kmttg);
+                  config.gui.setTitle(title);
+                  config.gui.progressBar_setValue(pct);                  
+               }
+               if (pct == -2) {
+                  String title = String.format("encode: %s %s", t, config.kmttg);
+                  config.gui.setTitle(title);                     
+               }
+            } else {
+               if (pct > -1)
+                  status = String.format("%d%%",pct);
+            }            
+            // Update STATUS column
+            if (size != null) status += "---" + size;               
+            config.gui.jobTab_UpdateJobMonitorRowStatus(job, status);
          }
-        return true;
+         return true;
       } else {
          // Job finished
          if (config.GUI) {
@@ -316,6 +315,24 @@ public class encode implements Serializable {
       if (last.contains("Encoding")) {
          String[] all = last.split("Encoding");
          String pct_str = all[all.length-1].replaceFirst("^.+,\\s+(.+)\\s+%.*$", "$1");
+         try {
+            return (int)Float.parseFloat(pct_str);
+         }
+         catch (NumberFormatException n) {
+            return 0;
+         }
+      }
+      return 0;
+   }
+
+   // Obtain pct complete from handbrake stdout
+   private int mencoderGetPct() {
+      String last = process.getStdoutLast();
+      if (last.matches("")) return 0;
+      Pattern pat = Pattern.compile("(\\d+)%");
+      Matcher match = pat.matcher(last);
+      if (match.find()) {
+         String pct_str = match.group(1);
          try {
             return (int)Float.parseFloat(pct_str);
          }
