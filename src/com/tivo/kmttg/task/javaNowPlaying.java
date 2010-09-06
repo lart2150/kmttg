5e31c88ba633f81b1395cda51e5065a256acdc32
moyekj@yahoo.com <moyekj@yahoo.com@b0df3fdd-2057-4f73-b226-dfc9190e8f7a>
2010-09-06 06:28:20 +0000

    Added as alternative to curl method
    git-svn-id: https://svn.code.sf.net/p/kmttg/code/trunk@1188 b0df3fdd-2057-4f73-b226-dfc9190e8f7a

---
+++ src/com/tivo/kmttg/task/javaNowPlaying.java
@@ -0,0 +1,274 @@
+package com.tivo.kmttg.task;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Serializable;
+import java.util.Hashtable;
+import java.util.Stack;
+
+import com.tivo.kmttg.main.auto;
+import com.tivo.kmttg.main.config;
+import com.tivo.kmttg.main.http;
+import com.tivo.kmttg.main.jobData;
+import com.tivo.kmttg.main.jobMonitor;
+import com.tivo.kmttg.util.backgroundProcess;
+import com.tivo.kmttg.util.debug;
+import com.tivo.kmttg.util.file;
+import com.tivo.kmttg.util.log;
+import com.tivo.kmttg.util.parseNPL;
+
+public class javaNowPlaying implements Serializable {
+   private static final long serialVersionUID = 1L;
+   private Thread thread = null;
+   private Boolean thread_running = false;
+   private Boolean success = false;
+   private Stack<Hashtable<String,String>> ENTRIES = new Stack<Hashtable<String,String>>();
+   private String outputFile = "";
+   private int AnchorOffset = 0;
+   private int TotalItems = 0;
+   private backgroundProcess process;
+   public  jobData job;
+
+   public javaNowPlaying(jobData job) {
+      debug.print("job=" + job);
+      this.job = job;
+      
+      // Generate unique outputFile name
+      outputFile = file.makeTempFile("NPL");
+   }
+      
+   public backgroundProcess getProcess() {
+      return process;
+   }
+   
+   public static Boolean submitJob(String tivoName) {
+      debug.print("");
+      String ip = config.TIVOS.get(tivoName);
+      if (ip == null || ip.length() == 0) {
+         log.error("IP not defined for tivo: " + tivoName);
+         return false;
+      }
+      jobData job = new jobData();
+      job.tivoName           = tivoName;
+      job.type               = "javaplaylist";
+      job.name               = "java";
+      job.ip                 = ip;
+      jobMonitor.submitNewJob(job);
+      return true;      
+   }
+   
+   public Boolean start() {
+      debug.print("");
+      if (job.ip == null || job.ip.length() == 0) {
+         log.error("IP not defined for tivo: " + job.tivoName);
+         jobMonitor.removeFromJobList(job);
+         return false;
+      }
+      if (config.MAK == null || config.MAK.length() != 10) {
+         log.error("MAK not specified or not correct");
+         return false;
+      }
+      
+      String urlString = "https://";
+      urlString += job.ip;
+      // Enable testLimit only for testing multiple downloads
+      Boolean testLimit = false;
+      if (testLimit) {
+         urlString += "/TiVoConnect?Command=QueryContainer&Container=/NowPlaying&Recurse=Yes&ItemCount=5&AnchorOffset=";
+      } else {
+         urlString += "/TiVoConnect?Command=QueryContainer&Container=/NowPlaying&Recurse=Yes&AnchorOffset=";
+      }
+      urlString += AnchorOffset;
+      final String url = urlString;
+      if (AnchorOffset == 0)
+         log.print(">> Getting Now Playing List from " + job.tivoName + " ...");
+      else
+         log.print(">> Continuing Now Playing List from " + job.tivoName + " (" + AnchorOffset + "/" + TotalItems + ")...");
+      // Run download method in a separate thread
+      Runnable r = new Runnable() {
+         public void run () {
+            try {
+               success = getNPL(url, "tivo", config.MAK);
+               thread_running = false;
+            }
+            catch (InterruptedException ie) {
+               thread_running = false;
+               Thread.currentThread().interrupt();
+            }
+         }
+      };
+      thread_running = true;
+      thread = new Thread(r);
+      thread.start();
+
+      return true;
+   }
+   
+   public void kill() {
+      debug.print("");
+      thread.interrupt();
+      log.warn("Killing '" + job.type + "' TiVo: " + job.tivoName);
+      thread_running = false;
+   }
+   
+   // Check status of a currently running job
+   // Returns true if still running, false if job finished
+   public Boolean check() {
+      if (thread_running) {
+         // Still running
+         if (config.GUI) {
+            // Update STATUS column
+            String t = jobMonitor.getElapsedTime(job.time);
+            config.gui.jobTab_UpdateJobMonitorRowStatus(job, t);
+            if ( jobMonitor.isFirstJobInMonitor(job) ) {
+               String title = String.format("playlist: %s %s", t, config.kmttg);
+               config.gui.setTitle(title);
+            }
+         }
+         return true;
+      } else {
+         // Job finished
+         if (config.GUI) {
+            if ( jobMonitor.isFirstJobInMonitor(job) ) {
+               config.gui.setTitle(config.kmttg);
+            }
+         }
+         
+         // NOTE: Not removing from job list yet as there could be more runs needed
+         
+         // Check for problems
+         int failed = 0;
+         
+         if ( ! success ) {
+            failed = 1;
+         }
+         
+         // No or empty output means problems         
+         if ( file.isEmpty(outputFile) ) {
+            failed = 1;
+         }
+         
+         // Check that first line is xml
+         if (failed == 0) {
+            try {
+               BufferedReader xml = new BufferedReader(new FileReader(outputFile));
+               String first = xml.readLine();
+               if ( ! first.toLowerCase().matches("^.+xml.+$") ) {
+                  failed = 1;
+                  log.error(first);
+               }
+               xml.close();
+            }
+            catch (IOException ex) {
+               failed = 1;
+            }
+         }
+         
+         if (failed == 1) {
+            log.error("Failed to retrieve Now Playing List from " + job.tivoName);
+            log.error("Check YOUR MAK & IP settings");
+            process.printStderr();
+            jobMonitor.removeFromJobList(job);
+         } else {
+            log.warn("NPL job completed: " + jobMonitor.getElapsedTime(job.time));
+            log.print("---DONE--- job=" + job.type + " tivo=" + job.tivoName);
+            
+            // Success, so parse the result
+            return parseNPL(outputFile);
+         }
+      }
+      file.delete(outputFile);
+      
+      return false;
+   }
+
+   // Return true if additional downloads needed, false otherwise
+   // NOTE: Must use UTF8 for special characters like Spanish/French characters
+   private Boolean parseNPL(String file) {
+      debug.print("file=" + file);
+      Hashtable<String,Integer> result = parseNPL.parseFile(file, job.tivoName, ENTRIES);
+      if (result == null) {
+         com.tivo.kmttg.util.file.delete(outputFile);
+         return false;
+      }
+      TotalItems = result.get("TotalItems");
+      
+      if ( (AnchorOffset + result.get("ItemCount")) < result.get("TotalItems") ) {
+         // Additional items to retrieve
+         AnchorOffset += result.get("ItemCount");
+         start();
+         return true;
+      } else {
+         // Done
+         jobMonitor.removeFromJobList(job);
+         if (config.GUI_AUTO > 0) {
+            // Clear NPL
+            //config.gui.nplTab_clear(job.tivoName);
+            // Update NPL
+            config.gui.nplTab_SetNowPlaying(job.tivoName, ENTRIES);
+            
+            // Match auto keywords against entries
+            int count = 0;
+            for (int j=0; j<ENTRIES.size(); j++) {
+               if ( auto.keywordSearch(ENTRIES.get(j)) )
+                  count++;
+            }
+            log.print("TOTAL auto matches for '" + job.tivoName + "' = " + count + "/" + ENTRIES.size());
+            config.GUI_AUTO--;
+         }
+         else if (config.GUI) {
+            // GUI mode: populate NPL table
+            config.gui.nplTab_SetNowPlaying(job.tivoName, ENTRIES);
+         } else {
+            // Batch mode
+            int count = 0;
+            for (int j=0; j<ENTRIES.size(); j++) {
+               if ( auto.keywordSearch(ENTRIES.get(j)) )
+                  count++;
+            }
+            log.print("TOTAL auto matches for '" + job.tivoName + "' = " + count + "/" + ENTRIES.size());
+         }
+         com.tivo.kmttg.util.file.delete(outputFile);
+         return false;
+      }
+   }
+   
+   private Boolean getNPL(String url, String username, String password) throws InterruptedException {
+      debug.print("url=" + url);
+      InputStream in = http.getNowPlaying(url, username, password);
+      if (in == null) {
+         return false;
+      } else {
+         int BUFSIZE = 65536;
+         byte[] buffer = new byte[BUFSIZE];
+         int c;
+         try {
+            FileOutputStream out = new FileOutputStream(outputFile);
+            while ((c = in.read(buffer, 0, BUFSIZE)) != -1) {
+               if (Thread.interrupted()) {
+                  out.close();
+                  in.close();
+                  throw new InterruptedException();
+               }
+               out.write(buffer, 0, c);
+            }
+            out.close();
+            in.close();
+         }
+         catch (FileNotFoundException e) {
+            log.error(e.getMessage());
+            return false;
+         }
+         catch (IOException e) {
+            log.error(e.getMessage());
+            return false;
+         }
+      }
+
+      return true;
+   }
+}
---
+++ src/com/tivo/kmttg/task/javadownload.java
@@ -0,0 +1,254 @@
+package com.tivo.kmttg.task;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Serializable;
+import java.util.Date;
+
+import com.tivo.kmttg.main.auto;
+import com.tivo.kmttg.main.config;
+import com.tivo.kmttg.main.http;
+import com.tivo.kmttg.main.jobData;
+import com.tivo.kmttg.main.jobMonitor;
+import com.tivo.kmttg.util.backgroundProcess;
+import com.tivo.kmttg.util.debug;
+import com.tivo.kmttg.util.file;
+import com.tivo.kmttg.util.log;
+import com.tivo.kmttg.util.string;
+
+public class javadownload implements Serializable {
+   private static final long serialVersionUID = 1L;
+   private Thread thread = null;
+   private Boolean thread_running = false;
+   private Boolean success = false;
+   private backgroundProcess process;
+   public jobData job;
+
+   public javadownload(jobData job) {
+      debug.print("job=" + job);
+      this.job = job;
+   }
+   
+   public backgroundProcess getProcess() {
+      return process;
+   }
+   
+   public Boolean launchJob() {
+      debug.print("");
+      Boolean schedule = true;
+      if ( file.isFile(job.tivoFile) ) {
+         if (config.OverwriteFiles == 0) {
+            log.warn("SKIPPING DOWNLOAD, FILE ALREADY EXISTS: " + job.tivoFile);
+            schedule = false;
+         } else {
+            log.warn("OVERWRITING EXISTING FILE: " + job.tivoFile);
+            file.delete(job.tivoFile);
+         }
+      }
+
+      if (schedule) {
+         // Create sub-folders for output file if needed
+         if ( ! jobMonitor.createSubFolders(job.tivoFile, job) ) schedule = false;
+      }
+      
+      if (schedule) {
+         if ( start() ) {
+            job.process_javadownload = this;
+            jobMonitor.updateJobStatus(job, "running");
+            job.time = new Date().getTime();
+         }
+         return true;
+      } else {
+         return false;
+      }      
+   }
+   
+   private Boolean start() {
+      debug.print("");
+      // Run download method in a separate thread
+      Runnable r = new Runnable() {
+         public void run () {
+            try {
+               success = download();
+               thread_running = false;
+            }
+            catch (InterruptedException ie) {
+               thread_running = false;
+               Thread.currentThread().interrupt();
+            }
+         }
+      };
+      thread_running = true;
+      thread = new Thread(r);
+      thread.start();
+
+      return true;
+   }
+   
+   public void kill() {
+      debug.print("");
+      thread.interrupt();
+      log.warn("Killing '" + job.type + "' file: " + job.tivoFile);
+      thread_running = false;
+   }
+   
+   // Check status of a currently running job
+   // Returns true if still running, false if job finished
+   public Boolean check() {
+      if (thread_running) {
+         // Still running
+         if (config.GUI && file.isFile(job.tivoFile)) {
+            // Update status in job table
+            Long size = file.size(job.tivoFile);
+            String s = String.format("%.2f MB", (float)size/Math.pow(2,20));
+            String t = jobMonitor.getElapsedTime(job.time);
+            int pct = Integer.parseInt(String.format("%d", size*100/job.tivoFileSize));
+            
+            // Calculate current transfer rate over last dt msecs
+            Long dt = (long)5000;
+            job.time2 = new Date().getTime();
+            job.size2 = size;
+            if (job.time1 == null)
+               job.time1 = job.time2;
+            if (job.size1 == null)
+               job.size1 = job.size2;
+            if (job.time2-job.time1 >= dt && job.size2 > job.size1) {
+               job.rate = getRate(job.size2-job.size1, job.time2-job.time1);
+               job.time1 = job.time2;
+               job.size1 = job.size2;
+            }
+            
+            if ( jobMonitor.isFirstJobInMonitor(job) ) {
+               // Update STATUS column 
+               config.gui.jobTab_UpdateJobMonitorRowStatus(job, t + "---" + s + "---" + job.rate);
+               
+               // If 1st job then update title & progress bar
+               String title = String.format("download: %d%% %s", pct, config.kmttg);
+               config.gui.setTitle(title);
+               config.gui.progressBar_setValue(pct);
+            } else {
+               // Update STATUS column            
+               config.gui.jobTab_UpdateJobMonitorRowStatus(job, String.format("%d%%",pct) + "---" + s + "---" + job.rate);
+            }
+         }
+         return true;
+      } else {
+         // Job finished
+         if (config.GUI) {
+            if ( jobMonitor.isFirstJobInMonitor(job) ) {
+               config.gui.setTitle(config.kmttg);
+               config.gui.progressBar_setValue(0);
+            }
+         }
+         
+         jobMonitor.removeFromJobList(job);
+         
+         // Check for problems
+         int failed = 0;
+         
+         if ( ! success ) {
+            failed = 1;
+         }
+         
+         // No or empty output means problems         
+         if ( file.isEmpty(job.tivoFile) ) {
+            failed = 1;
+         } else {
+            // Print statistics for the job
+            String s = String.format("%.2f MB", file.size(job.tivoFile)/Math.pow(2,20));
+            String t = jobMonitor.getElapsedTime(job.time);
+            String r = jobMonitor.getRate(file.size(job.tivoFile), job.time);
+            log.warn(job.tivoFile + ": size=" + s + " elapsed=" + t + " (" + r + ")");
+         }
+         
+         // Check first line in tivo file for errors
+         // Also if file size is very small then it's likely a failure
+         if (failed == 0) {
+            try {
+               BufferedReader ifp = new BufferedReader(new FileReader(job.tivoFile));
+               String first = ifp.readLine();
+               ifp.close();
+               if ( first.toLowerCase().contains("html") ) failed = 1;
+               if ( first.toLowerCase().contains("busy") ) failed = 1;
+               if ( first.toLowerCase().contains("failed") ) failed = 1;
+               if ( file.size(job.tivoFile) < 1000 ) failed = 1;
+               if (failed == 1) {
+                  process.printStdout();
+                  log.error(first);
+               }
+            }
+            catch (IOException ex) {
+               failed = 1;
+            }
+         }
+         
+         if (failed == 1) {
+            log.error("Download failed to file: " + job.tivoFile);
+            file.delete(job.tivoFile);
+            
+            // Try download again with delayed launch time if specified
+            if (job.launch_tries < config.download_tries) {
+               job.launch_tries++;
+               log.warn(string.basename(job.tivoFile) + ": Download attempt # " +
+                     job.launch_tries + " scheduled in " + config.download_retry_delay + " seconds.");
+               job.launch_time = new Date().getTime() + config.download_retry_delay*1000;
+               jobMonitor.submitNewJob(job);
+            } else {
+               log.error(string.basename(job.tivoFile) + ": Too many failed downloads, GIVING UP!!");
+            }
+         } else {
+            log.print("---DONE--- job=" + job.type + " output=" + job.tivoFile);
+            // Add auto history entry if auto downloads configured
+            if (file.isFile(config.autoIni))
+               auto.AddHistoryEntry(job);
+         }
+      }
+      return false;
+   }
+
+   // Return rate in Mbps (ds=delta bytes, dt=delta time in msecs)
+   private String getRate(long ds, long dt) {      
+      return String.format("%.1f Mbps", (ds*8000)/(1e6*dt));
+   }
+   
+   private Boolean download() throws InterruptedException {
+      String url = job.url;
+      if (config.TSDownload == 1)
+         url += "&Format=video/x-tivo-mpeg-ts";
+      InputStream in = http.getTivoStream(url, "tivo", config.MAK);
+      if (in == null) {
+         return false;
+      } else {
+         int BUFSIZE = 65536;
+         byte[] buffer = new byte[BUFSIZE];
+         int c;
+         try {
+            FileOutputStream out = new FileOutputStream(job.tivoFile);
+            while ((c = in.read(buffer, 0, BUFSIZE)) != -1) {
+               if (Thread.interrupted()) {
+                  out.close();
+                  in.close();
+                  throw new InterruptedException();
+               }
+               out.write(buffer, 0, c);
+            }
+            out.close();
+            in.close();
+         }
+         catch (FileNotFoundException e) {
+            log.error(e.getMessage());
+            return false;
+         }
+         catch (IOException e) {
+            log.error(e.getMessage());
+            return false;
+         }
+      }
+
+      return true;
+   }
+}
