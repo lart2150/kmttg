dcca11093dbbfc9b0e3c7ea6251cf397c684eb25
moyekj@yahoo.com <moyekj@yahoo.com@b0df3fdd-2057-4f73-b226-dfc9190e8f7a>
2010-09-27 23:55:04 +0000

    Null exception prevention for SetEncodings
    git-svn-id: https://svn.code.sf.net/p/kmttg/code/trunk@1255 b0df3fdd-2057-4f73-b226-dfc9190e8f7a

--- src/com/tivo/kmttg/gui/gui.java
+++ src/com/tivo/kmttg/gui/gui.java
@@ -1260,18 +1260,20 @@ public class gui {
    public void SetEncodings(Stack<String> values) {
       debug.print("values=" + values);
       
-      // Get existing setting in combobox
-      String current = null;
-      if (encoding.getComponentCount() > 0) {
-         current = (String)encoding.getSelectedItem();
+      if (encoding != null) {      
+         // Get existing setting in combobox
+         String current = null;
+         if (encoding.getComponentCount() > 0) {
+            current = (String)encoding.getSelectedItem();
+         }
+         String[] names = new String[values.size()];
+         for (int i=0; i<values.size(); ++i) {
+            names[i] = values.get(i);
+         }
+         combobox.SetValues(encoding, names);
+         if (current != null)
+            encoding.setSelectedItem(current);
       }
-      String[] names = new String[values.size()];
-      for (int i=0; i<values.size(); ++i) {
-         names[i] = values.get(i);
-      }
-      combobox.SetValues(encoding, names);
-      if (current != null)
-         encoding.setSelectedItem(current);
    }
    
    public String GetSelectedEncoding() {
