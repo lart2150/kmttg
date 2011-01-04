f8c87a5cf663cd92492f8101fb6b9ce8a0a76a16
moyekj@yahoo.com <moyekj@yahoo.com@b0df3fdd-2057-4f73-b226-dfc9190e8f7a>
2011-01-04 03:21:52 +0000

    Pass in proc2 so can destroy it when appropriate
    git-svn-id: https://svn.code.sf.net/p/kmttg/code/trunk@1316 b0df3fdd-2057-4f73-b226-dfc9190e8f7a

--- src/com/tivo/kmttg/util/PipeHandler.java
+++ src/com/tivo/kmttg/util/PipeHandler.java
@@ -6,10 +6,12 @@ import java.io.OutputStream;
 public class PipeHandler extends Thread {
    private final InputStream in;
    private final OutputStream out;
+   private final Process proc2;
       
-   PipeHandler(InputStream in, OutputStream out) {
+   PipeHandler(InputStream in, Process proc2) {
      this.in = in;
-     this.out = out;
+     this.out = proc2.getOutputStream();
+     this.proc2 = proc2;
    }
    
    public void run() {
@@ -20,11 +22,14 @@ public class PipeHandler extends Thread {
            if (Thread.interrupted()) {
               out.close();
               in.close();
+              proc2.destroy();
               throw new InterruptedException("Killed by user");
            }
            out.write(buf, 0, i);
            out.flush();
         }
+        // Kill proc2 now that InputStream is complete
+        proc2.destroy();
      } catch(Exception e) {
         log.error("PipeHandler exception: " + e.getMessage());
      }
