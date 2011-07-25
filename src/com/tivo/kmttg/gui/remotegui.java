138af5fc926187ceae30090eb1539822ba413b25
moyekj@yahoo.com <moyekj@yahoo.com@b0df3fdd-2057-4f73-b226-dfc9190e8f7a>
2011-07-25 01:28:12 +0000

    Remove unused imports
    git-svn-id: https://svn.code.sf.net/p/kmttg/code/trunk@1583 b0df3fdd-2057-4f73-b226-dfc9190e8f7a

--- src/com/tivo/kmttg/gui/remotegui.java
+++ src/com/tivo/kmttg/gui/remotegui.java
@@ -20,9 +20,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.util.ArrayList;
 import java.util.Hashtable;
-import java.util.List;
 import java.util.Stack;
 
 import javax.swing.AbstractAction;
