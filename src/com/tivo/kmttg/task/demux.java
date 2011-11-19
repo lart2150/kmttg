432041f0237f0603c7f5f1209d399deb8ffffd02
moyekj@yahoo.com <moyekj@yahoo.com@b0df3fdd-2057-4f73-b226-dfc9190e8f7a>
2011-11-19 22:24:31 +0000

    Moved util methods
    git-svn-id: https://svn.code.sf.net/p/kmttg/code/trunk@1794 b0df3fdd-2057-4f73-b226-dfc9190e8f7a

--- src/com/tivo/kmttg/task/demux.java
+++ src/com/tivo/kmttg/task/demux.java
@@ -1,18 +1,13 @@
 package com.tivo.kmttg.task;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.Stack;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import com.tivo.kmttg.main.config;
 import com.tivo.kmttg.main.jobData;
 import com.tivo.kmttg.main.jobMonitor;
+import com.tivo.kmttg.util.ProjectX;
 import com.tivo.kmttg.util.backgroundProcess;
 import com.tivo.kmttg.util.debug;
 import com.tivo.kmttg.util.file;
@@ -101,7 +96,7 @@ public class demux implements Serializable {
       process.kill();
       log.warn("Killing '" + job.type + "' job: " + process.toString());
       // Clean up all the intermediate files left by projectx
-      cleanUpTempFiles(job.mpegFile);
+      ProjectX.cleanUpTempFiles(job.mpegFile);
    }
 
    // Check status of a currently running job
@@ -114,7 +109,7 @@ public class demux implements Serializable {
          // Still running
          if (config.GUIMODE) {
             // Update STATUS column
-            int pct = projectxGetPct();
+            int pct = ProjectX.projectxGetPct(process);
             if ( pct > -1 ) {               
                // Update status in job table
                String status = String.format("%d%%",pct);
@@ -148,7 +143,7 @@ public class demux implements Serializable {
             failed = 1;
          }
          
-         Stack<String> outputFiles = getOutputFiles(logFile);
+         Stack<String> outputFiles = ProjectX.getOutputFiles(logFile);
          if (outputFiles == null) {
             log.error("No demux output files found");
             failed = 1;
@@ -184,91 +179,5 @@ public class demux implements Serializable {
       return false;
    }
 
-   // Obtain pct complete from projectx stdout
-   private int projectxGetPct() {
-      String last = process.getStdoutLast();
-      if (last.matches("")) return -1;
-      Pattern pat = Pattern.compile("(\\d+)\\s*%");
-      Matcher match = pat.matcher(last);
-      if (match.find()) {
-         String pct_str = match.group(1);
-         try {
-            return (int)Float.parseFloat(pct_str);
-         }
-         catch (NumberFormatException n) {
-            return -1;
-         }
-      }
-      return -1;
-   }
-   
-   // Parse projectx demux log file looking for all output files
-   // ---> new File: ./sample.m2v
-   // ---> new File: './sample.ac3'
-   private Stack<String> getOutputFiles(String logFile) {
-      Stack<String> outputFiles = new Stack<String>();
-      try {
-         BufferedReader ini = new BufferedReader(new FileReader(logFile));
-         String line = null;
-         Pattern p = Pattern.compile("^.+new\\s+File:\\s+(.+)$");
-         Matcher m;
-         Boolean print = false;
-         while (( line = ini.readLine()) != null) {
-            // Get rid of leading and trailing white space
-            line = line.replaceFirst("^\\s*(.*$)", "$1");
-            line = line.replaceFirst("^(.*)\\s*$", "$1");
-            if (line.length() == 0) continue; // skip empty lines
-            if (line.startsWith("summary"))
-               print = true;
-            if (print)
-               log.print(line);
-            m = p.matcher(line);
-            if (m.matches()) {
-               String f = m.group(1);
-               // Strip off quotes
-               f = f.replaceAll("'", "");
-               f = f.replaceAll("\"", "");
-               outputFiles.add(f);
-            }
-         }
-         ini.close();
-         if (outputFiles.size() == 0)
-            return null;
-      }         
-      catch (IOException ex) {
-         log.error("Problem parsing demux log file: " + logFile);
-         return null;
-      }
-
-      return outputFiles;
-   }
-
-   // Clean up temp files created by projectx
-   // These files are in format for example: long_timestamp_issue.$ppes$1-E3F464
-   private void cleanUpTempFiles(String fileName) {
-      File dir = new File(string.dirname(fileName));
-      File[] listOfFiles = dir.listFiles();
-      String prefix = string.replaceSuffix(string.basename(fileName), "");
-      Pattern p = Pattern.compile("^" + prefix + "\\.\\$.+$");
-      Matcher m;
-      if (listOfFiles != null) {
-         for(int i=0; i<listOfFiles.length; ++i) {
-            if (listOfFiles[i].isFile()) {
-               m = p.matcher(listOfFiles[i].getName());
-               if (m.matches()) {
-                  if ( ! file.delete(listOfFiles[i].getAbsolutePath()) ) {
-                     try {
-                        // Sleep 1 second and try deleting again
-                        Thread.sleep(1000);
-                        file.delete(listOfFiles[i].getAbsolutePath());
-                     } catch (InterruptedException e) {
-                        log.error(e.getMessage());
-                     }
-                  }
-               }
-            }
-         }
-      }
-   }
 
 }
