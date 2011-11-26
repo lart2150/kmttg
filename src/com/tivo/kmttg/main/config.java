b98b31f3babe7e854d8d89a77df676f507fbbe7c
moyekj@yahoo.com <moyekj@yahoo.com@b0df3fdd-2057-4f73-b226-dfc9190e8f7a>
2011-11-26 14:40:46 +0000

    Changed to debug release
    git-svn-id: https://svn.code.sf.net/p/kmttg/code/trunk@1813 b0df3fdd-2057-4f73-b226-dfc9190e8f7a

--- src/com/tivo/kmttg/main/config.java
+++ src/com/tivo/kmttg/main/config.java
@@ -17,7 +17,7 @@ import com.tivo.kmttg.util.*;
 import com.tivo.kmttg.gui.gui;
 
 public class config {
-   public static String kmttg = "kmttg v0p8l";
+   public static String kmttg = "kmttg v0p8m_beta";
    
    // encoding related
    public static String encProfDir = "";
