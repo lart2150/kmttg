51199d20238cb58fc188453747a5555017832595
moyekj@yahoo.com <moyekj@yahoo.com@b0df3fdd-2057-4f73-b226-dfc9190e8f7a>
2012-01-27 00:41:04 +0000

    Changed to perform demux/remux as 1 task
    git-svn-id: https://svn.code.sf.net/p/kmttg/code/trunk@1872 b0df3fdd-2057-4f73-b226-dfc9190e8f7a

--- src/com/tivo/kmttg/task/projectxcut.java
+++ src/com/tivo/kmttg/task/projectxcut.java
@@ -18,6 +18,9 @@ public class projectxcut implements Serializable {
    private static final long serialVersionUID = 1L;
    private backgroundProcess process;
    private jobData job;
+   private Boolean remux = false;
+   private String mpegFile;
+   private long totalSize = 0L;
 
    // constructor
    public projectxcut(jobData job) {
@@ -113,88 +116,208 @@ public class projectxcut implements Serializable {
    // Returns true if still running, false if job finished
    // If job is finished then check result
    public Boolean check() {
-      //debug.print("");
-      int exit_code = process.exitStatus();
-      if (exit_code == -1) {
-         // Still running
-         if (config.GUIMODE) {
-            int pct = ProjectX.projectxGetPct(process);
-            if ( pct > -1 ) {               
+      if (! remux) {
+         // THIS IS DEMUX JOB
+         //debug.print("");
+         int exit_code = process.exitStatus();
+         if (exit_code == -1) {
+            // Still running
+            if (config.GUIMODE) {
+               int pct = ProjectX.projectxGetPct(process);
+               if ( pct > -1 ) {               
+                  // Update status in job table
+                  String status = String.format("%d%%",pct);
+                  config.gui.jobTab_UpdateJobMonitorRowStatus(job, status);
+                  
+                  if ( jobMonitor.isFirstJobInMonitor(job) ) {
+                     // If 1st job then update title & progress bar
+                     String title = String.format("projectxcut: %s %s", status, config.kmttg);
+                     config.gui.setTitle(title);
+                     config.gui.progressBar_setValue(pct);
+                  }
+               }
+            }
+           return true;
+         } else {
+            // Job finished         
+            if ( jobMonitor.isFirstJobInMonitor(job) ) {
+               config.gui.setTitle(config.kmttg);
+               config.gui.progressBar_setValue(0);
+            }
+            
+            // Check for problems
+            int failed = 0;
+            
+            // No or empty logFile means problems
+            String logFile = string.replaceSuffix(job.mpegFile, "_log.txt");
+            if ( ! file.isFile(logFile) || file.isEmpty(logFile) ) {
+               log.error("Unable to find projectxcut output log file: " + logFile);
+               failed = 1;
+            }
+            
+            Stack<String> outputFiles = ProjectX.getOutputFiles(logFile);
+            if (outputFiles == null) {
+               log.error("No projectxcut output files found");
+               failed = 1;
+            }
+            job.demuxFiles = outputFiles;
+            
+            // exit status != 0 => problem
+            if (exit_code != 0) {
+               failed = 1;
+            }
+            
+            if (failed == 1) {
+               log.error("projectxcut failed (exit code: " + exit_code + " ) - check command: " + process.toString());
+               process.printStderr();
+               jobMonitor.removeFromJobList(job);
+               return false;
+            } else {
+               log.warn("projectx cut demux completed: " + jobMonitor.getElapsedTime(job.time));
+               log.print("---DONE--- job=" + job.type + "\ndemux output files:");
+               for (int i=0; i<outputFiles.size(); ++i)
+                  log.print(outputFiles.get(i));
+               file.delete(logFile);
+               
+               // Remove Ad Cut files if option enabled
+               if ( config.RemoveComcutFiles == 1 ) {
+                  if (file.delete(job.vprjFile))
+                     log.print("(Deleted vprj file: " + job.vprjFile + ")");
+                  if (file.delete(job.edlFile))
+                     log.print("(Deleted edl file: " + job.edlFile + ")");
+                  if (file.delete(job.xclFile))
+                     log.print("(Deleted xcl file: " + job.xclFile + ")");
+               }
+               
+               // Start remux background job               
+               mpegFile = job.mpegFile_cut;
+               job.mpegFile_fix = job.mpegFile_cut + ".qsfix";               
+               totalSize = ProjectX.getDemuxFilesSize(job.demuxFiles);
+
+               Stack<String> command = new Stack<String>();
+               command.add(config.ffmpeg);
+               command.add("-y");
+               command.add("-fflags");
+               command.add("genpts");
+               for (int i=0; i<job.demuxFiles.size(); ++i) {
+                  command.add("-i");
+                  command.add(job.demuxFiles.get(i));
+               }
+               command.add("-acodec");
+               command.add("copy");
+               command.add("-vcodec");
+               command.add("copy");
+               command.add("-f");
+               command.add("dvd");
+               command.add(job.mpegFile_fix);
+               process = new backgroundProcess();
+               log.print(">> Running ffmpeg remux to generate " + job.mpegFile_fix + " ...");
+               if ( process.run(command) ) {
+                  log.print(process.toString());
+               } else {
+                  log.error("Failed to start command: " + process.toString());
+                  process.printStderr();
+                  process = null;
+                  jobMonitor.removeFromJobList(job);
+                  return false;
+               }
+               remux = true;
+               return true;
+            }
+         }
+      } else {
+         // THIS IS REMUX JOB
+         int exit_code = process.exitStatus();
+         if (exit_code == -1) {
+            // Still running
+            if (config.GUIMODE && totalSize > 0) {
+               // Update STATUS column
+               int pct = Integer.parseInt(String.format("%d", file.size(job.mpegFile_fix)*100/totalSize));
                // Update status in job table
                String status = String.format("%d%%",pct);
                config.gui.jobTab_UpdateJobMonitorRowStatus(job, status);
                
                if ( jobMonitor.isFirstJobInMonitor(job) ) {
                   // If 1st job then update title & progress bar
-                  String title = String.format("projectxcut: %s %s", status, config.kmttg);
+                  String title = String.format("remux: %s %s", status, config.kmttg);
                   config.gui.setTitle(title);
                   config.gui.progressBar_setValue(pct);
                }
             }
-         }
-        return true;
-      } else {
-         // Job finished         
-         if ( jobMonitor.isFirstJobInMonitor(job) ) {
-            config.gui.setTitle(config.kmttg);
-            config.gui.progressBar_setValue(0);
-         }
-         jobMonitor.removeFromJobList(job);
-         
-         // Check for problems
-         int failed = 0;
-         
-         // No or empty logFile means problems
-         String logFile = string.replaceSuffix(job.mpegFile, "_log.txt");
-         if ( ! file.isFile(logFile) || file.isEmpty(logFile) ) {
-            log.error("Unable to find projectxcut output log file: " + logFile);
-            failed = 1;
-         }
-         
-         Stack<String> outputFiles = ProjectX.getOutputFiles(logFile);
-         if (outputFiles == null) {
-            log.error("No projectxcut output files found");
-            failed = 1;
-         }
-         
-         // exit status != 0 => problem
-         if (exit_code != 0) {
-            failed = 1;
-         }
-         
-         if (failed == 1) {
-            log.error("projectxcut failed (exit code: " + exit_code + " ) - check command: " + process.toString());
-            process.printStderr();
+           return true;
          } else {
-            log.warn("projectx job completed: " + jobMonitor.getElapsedTime(job.time));
-            log.print("---DONE--- job=" + job.type + "\ndemux output files:");
-            for (int i=0; i<outputFiles.size(); ++i)
-               log.print(outputFiles.get(i));
-            file.delete(logFile);
+            // Job finished         
+            if (config.GUIMODE) {
+               if ( jobMonitor.isFirstJobInMonitor(job) ) {
+                  config.gui.setTitle(config.kmttg);
+                  config.gui.progressBar_setValue(0);
+               }
+            }
+            jobMonitor.removeFromJobList(job);
             
-            // Remove Ad Cut files if option enabled
-            if ( config.RemoveComcutFiles == 1 ) {
-               if (file.delete(job.vprjFile))
-                  log.print("(Deleted vprj file: " + job.vprjFile + ")");
-               if (file.delete(job.edlFile))
-                  log.print("(Deleted edl file: " + job.edlFile + ")");
-               if (file.delete(job.xclFile))
-                  log.print("(Deleted xcl file: " + job.xclFile + ")");
+            // Check for problems
+            int failed = 0;
+            // No or empty mpegFile_fix means problems
+            if ( ! file.isFile(job.mpegFile_fix) || file.isEmpty(job.mpegFile_fix) ) {
+               log.error("Unable to find remux output file: " + job.mpegFile_fix);
+               failed = 1;
             }
             
-            // Schedule remux job
-            jobData new_job = new jobData();
-            new_job.source       = job.source;
-            new_job.tivoName     = job.tivoName;
-            new_job.type         = "remux";
-            new_job.name         = config.ffmpeg;
-            new_job.demuxFiles   = outputFiles;
-            new_job.mpegFile     = job.mpegFile;
-            new_job.mpegFile_cut = job.mpegFile_cut;
-            new_job.mpegFile_fix = job.mpegFile_cut + ".qsfix";
-            jobMonitor.submitNewJob(new_job);
+            // exit status != 0 => problem
+            if (exit_code != 0) {
+               failed = 1;
+            }
+            
+            if (failed == 1) {
+               log.error("remux failed (exit code: " + exit_code + " ) - check command: " + process.toString());
+               process.printStderr();
+            } else {
+               log.warn("remux job completed: " + jobMonitor.getElapsedTime(job.time));
+               log.print("---DONE--- job=" + job.type);
+               
+               // Remove the demuxed files
+               ProjectX.removeDemuxFiles(job.demuxFiles);
+               // Remove original .mpg file if this is remux for projectxcut and remove option enabled
+               if ( job.mpegFile_cut != null && config.RemoveComcutFiles_mpeg == 1 ) {
+                  if (file.delete(job.mpegFile))
+                     log.print("(Deleted mpeg file: " + job.mpegFile + ")");
+               }
+               
+               // Rename job.mpegFile_fix to mpegFile
+               Boolean result;
+               if (file.isFile(mpegFile)) {
+                  if (config.QSFixBackupMpegFile == 1) {
+                     // Rename mpegFile to backupFile if it exists
+                     String backupFile = mpegFile + ".bak";
+                     int count = 1;
+                     while (file.isFile(backupFile)) {
+                        backupFile = mpegFile + ".bak" + count++;
+                     }
+                     result = file.rename(mpegFile, backupFile);
+                     if ( result ) {
+                        log.print("(Renamed " + mpegFile + " to " + backupFile + ")");
+                     } else {
+                        log.error("Failed to rename " + mpegFile + " to " + backupFile);
+                        return false;                     
+                     }
+                  } else {
+                     // Remove mpegFile if it exists
+                     result = file.delete(mpegFile);
+                     if ( ! result ) {
+                        log.error("Failed to delete file in preparation for rename: " + mpegFile);
+                        return false;
+                     }
+                  }
+               }
+               // Now do the file rename
+               result = file.rename(job.mpegFile_fix, mpegFile);
+               if (result)
+                  log.print("(Renamed " + job.mpegFile_fix + " to " + mpegFile + ")");
+               else
+                  log.error("Failed to rename " + job.mpegFile_fix + " to " + mpegFile);
+            }
          }
+         return false;
       }
-      return false;
    }
 }
