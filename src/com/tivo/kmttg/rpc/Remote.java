bf5ed6ed80058a8f9fa11211e3cd62bfa8fe3831
moyekj@yahoo.com <moyekj@yahoo.com@b0df3fdd-2057-4f73-b226-dfc9190e8f7a>
2012-06-04 01:36:45 +0000

    Removed unused import
    git-svn-id: https://svn.code.sf.net/p/kmttg/code/trunk@1921 b0df3fdd-2057-4f73-b226-dfc9190e8f7a

--- src/com/tivo/kmttg/rpc/Remote.java
+++ src/com/tivo/kmttg/rpc/Remote.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.net.SocketException;
 import java.security.KeyManagementException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
