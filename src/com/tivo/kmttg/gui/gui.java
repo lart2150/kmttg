9808e6d6afbc1d6c3d5b21d4ad047f92d063f162
moyekj@yahoo.com <moyekj@yahoo.com@b0df3fdd-2057-4f73-b226-dfc9190e8f7a>
2012-10-27 17:37:53 +0000

    Save Deleted tab TiVo name
    git-svn-id: https://svn.code.sf.net/p/kmttg/code/trunk@2000 b0df3fdd-2057-4f73-b226-dfc9190e8f7a

--- src/com/tivo/kmttg/gui/gui.java
+++ src/com/tivo/kmttg/gui/gui.java
@@ -1529,7 +1529,7 @@ public class gui {
                }
             }
             if (remote_gui != null) {
-               String[]names = {"todo", "guide", "sp", "cancel", "search", "rc", "info", "premiere"};
+               String[]names = {"todo", "guide", "sp", "cancel", "deleted", "search", "rc", "info", "premiere"};
                ofp.write("\n<rpc_tivo>\n");
                for (int j=0; j<names.length; ++j)
                   ofp.write(names[j] + "=" + remote_gui.getTivoName(names[j]) + "\n");
