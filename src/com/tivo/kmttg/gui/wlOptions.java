9acaaa0cf83d237b8e0a6d6e6eef1305b48e226a
moyekj@yahoo.com <moyekj@yahoo.com@b0df3fdd-2057-4f73-b226-dfc9190e8f7a>
2012-11-15 07:47:00 +0000

    Added actor & director fields
    git-svn-id: https://svn.code.sf.net/p/kmttg/code/trunk@2044 b0df3fdd-2057-4f73-b226-dfc9190e8f7a

--- src/com/tivo/kmttg/gui/wlOptions.java
+++ src/com/tivo/kmttg/gui/wlOptions.java
@@ -12,8 +12,8 @@ import com.tivo.kmttg.util.log;
 
 public class wlOptions {
    JComponent[] components;
-   JLabel l_help, l_title, l_keyword, l_title_keyword;
-   JTextField tf_title, tf_keyword, tf_title_keyword;
+   JLabel l_help, l_title, l_keyword, l_title_keyword, l_actor, l_director;
+   JTextField tf_title, tf_keyword, tf_title_keyword, tf_actor, tf_director;
    JCheckBox cb_autorecord;
    
    public wlOptions() {      
@@ -21,13 +21,17 @@ public class wlOptions {
    }
    
    private void createComponents() {
-      l_help = new JLabel("MULTIPLE KEYWORD LOGIC:keywords1,keywords2 -keywords=>NOT, (keywords)=>OR");
+      l_help = new JLabel("KEYWORD LOGIC: keywords=>REQ, -keywords=>NOT, (keywords)=>OPT");
       l_title = new JLabel("Wishlist Title");
       tf_title = new JTextField(15);      
-      l_keyword = new JLabel("Keywords");
+      l_keyword = new JLabel("Keywords (keywords1,keywords2...)");
       tf_keyword = new JTextField(30);      
-      l_title_keyword = new JLabel("Title Keywords");
+      l_title_keyword = new JLabel("Title Keywords (keywords1,keywords2...)");
       tf_title_keyword = new JTextField(30);
+      l_actor = new JLabel("Actor (First Last,First2 Last2...)");
+      tf_actor = new JTextField(30);      
+      l_director = new JLabel("Director (First Last, First2 Last2...)");
+      tf_director = new JTextField(30);      
       cb_autorecord = new JCheckBox("Auto Record", false);
       
       components = new JComponent[] {
@@ -35,6 +39,8 @@ public class wlOptions {
          l_title, tf_title,
          l_keyword, tf_keyword,
          l_title_keyword, tf_title_keyword,
+         l_actor, tf_actor,
+         l_director, tf_director,
          cb_autorecord
       };
    }
@@ -51,16 +57,20 @@ public class wlOptions {
          String t = (String)tf_title.getText();
          String keyword = (String)tf_keyword.getText();
          String title_keyword = (String)tf_title_keyword.getText();
+         String actor = (String)tf_actor.getText();
+         String director = (String)tf_director.getText();
          
          // Check for minimum wishlist specification requirements
-         if (keyword.length() < 1 && title_keyword.length() < 1) {
-            log.error("Wishlist must contain keywords or title keywords");
+         if (keyword.length() < 1 && title_keyword.length() < 1 && actor.length() < 1 && director.length() < 1) {
+            log.error("Wishlist must contain at least 1 keyword");
             return null;
          }
          
          if (t.length() > 0) h.put("title", t);
          if (keyword.length() > 0) h.put("keyword", keyword);
          if (title_keyword.length() > 0) h.put("title_keyword", title_keyword);
+         if (actor.length() > 0) h.put("actor", actor);
+         if (director.length() > 0) h.put("director", director);
          if (cb_autorecord.isSelected())
             h.put("autorecord", "yes");
          return h;
@@ -72,9 +82,13 @@ public class wlOptions {
    private void setValues(Hashtable<String,String> h) {
       if (h.containsKey("title") && h.get("title").length() > 0)
          tf_title.setText(h.get("title"));
-      if (h.containsKey("keyword") && h.get("title").length() > 0)
+      if (h.containsKey("keyword") && h.get("keyword").length() > 0)
          tf_keyword.setText(h.get("keyword"));
-      if (h.containsKey("title_keyword") && h.get("title").length() > 0)
+      if (h.containsKey("title_keyword") && h.get("title_keyword").length() > 0)
          tf_title_keyword.setText(h.get("title_keyword"));
+      if (h.containsKey("actor") && h.get("actor").length() > 0)
+         tf_actor.setText(h.get("actor"));
+      if (h.containsKey("director") && h.get("director").length() > 0)
+         tf_director.setText(h.get("director"));
    }
 }
