4c11d93db86706b9fb6c18381322c20360315ff9
moyekj@yahoo.com <moyekj@yahoo.com@b0df3fdd-2057-4f73-b226-dfc9190e8f7a>
2013-01-01 22:21:50 +0000

    Added category option but disabled since IDs don't seem to work
    git-svn-id: https://svn.code.sf.net/p/kmttg/code/trunk@2221 b0df3fdd-2057-4f73-b226-dfc9190e8f7a

--- src/com/tivo/kmttg/gui/wlOptions.java
+++ src/com/tivo/kmttg/gui/wlOptions.java
@@ -3,12 +3,19 @@ package com.tivo.kmttg.gui;
 import java.util.Hashtable;
 
 import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
+import com.tivo.kmttg.JSON.JSONArray;
+import com.tivo.kmttg.JSON.JSONException;
+import com.tivo.kmttg.JSON.JSONObject;
+import com.tivo.kmttg.main.config;
+import com.tivo.kmttg.rpc.Remote;
 import com.tivo.kmttg.util.log;
+import com.tivo.kmttg.util.string;
 
 public class wlOptions {
    JComponent[] components;
@@ -16,6 +23,9 @@ public class wlOptions {
    JLabel l_help, l_title, l_keyword, l_title_keyword, l_actor, l_director;
    JTextField tf_title, tf_keyword, tf_title_keyword, tf_actor, tf_director;
    JCheckBox cb_autorecord;
+   JLabel l_categories = null;
+   JComboBox cb_categories = null;
+   JSONArray wishlistCategories = null;
    
    public wlOptions() {      
       createComponents();      
@@ -35,17 +45,59 @@ public class wlOptions {
       l_director = new JLabel("Director (First Last, First2 Last2...)");
       tf_director = new JTextField(30);      
       cb_autorecord = new JCheckBox("Auto Record", false);
+      // Intentionally disable for now since category Ids don't seem to work
+      //createCategories();
       
-      components = new JComponent[] {
-         label,
-         l_help,
-         l_title, tf_title,
-         l_keyword, tf_keyword,
-         l_title_keyword, tf_title_keyword,
-         l_actor, tf_actor,
-         l_director, tf_director,
-         cb_autorecord
-      };
+      if (cb_categories != null) {
+         components = new JComponent[] {
+            label,
+            l_help,
+            l_title, tf_title,
+            l_keyword, tf_keyword,
+            l_title_keyword, tf_title_keyword,
+            l_actor, tf_actor,
+            l_director, tf_director,
+            l_categories, cb_categories,
+            cb_autorecord
+         };
+      } else {
+         components = new JComponent[] {
+            label,
+            l_help,
+            l_title, tf_title,
+            l_keyword, tf_keyword,
+            l_title_keyword, tf_title_keyword,
+            l_actor, tf_actor,
+            l_director, tf_director,
+            cb_autorecord
+         };
+      }
+   }
+   
+   private void createCategories() {
+      if (wishlistCategories == null) {
+         wishlistCategories = getWishlistCategoryIds(config.getTivoNames().firstElement());
+      }
+      if (wishlistCategories != null) {
+         l_categories = new JLabel("Category");
+         cb_categories = new JComboBox();
+         cb_categories.addItem("");
+         try {
+            for (int i=0; i<wishlistCategories.length(); ++i) {
+               JSONObject json = wishlistCategories.getJSONObject(i);
+               if (json.has("subcategories")) {
+                  JSONArray a = json.getJSONArray("subcategories");
+                  for (int j=0; j<a.length(); ++j) {
+                     String name = json.getString("name") + ": ";
+                     name += a.getJSONObject(j).getString("name");
+                     cb_categories.addItem(name);
+                  }
+               }
+            }
+         } catch (JSONException e) {
+            log.error("createCategories - " + e.getMessage());
+         }
+      }
    }
    
    public Hashtable<String,String> promptUser(String title, Hashtable<String,String> h) {
@@ -65,9 +117,14 @@ public class wlOptions {
          String title_keyword = (String)tf_title_keyword.getText();
          String actor = (String)tf_actor.getText();
          String director = (String)tf_director.getText();
+         String category = (String)cb_categories.getSelectedItem();
          
          // Check for minimum wishlist specification requirements
-         if (keyword.length() < 1 && title_keyword.length() < 1 && actor.length() < 1 && director.length() < 1) {
+         if (keyword.length() < 1 &&
+             title_keyword.length() < 1 &&
+             actor.length() < 1 &&
+             director.length() < 1 &&
+             category.length() < 1) {
             log.error("Wishlist must contain at least 1 keyword");
             return null;
          }
@@ -77,6 +134,7 @@ public class wlOptions {
          if (title_keyword.length() > 0) h.put("title_keyword", title_keyword);
          if (actor.length() > 0) h.put("actor", actor);
          if (director.length() > 0) h.put("director", director);
+         if (category.length() > 0) h.put("categoryId", findCategoryId(category));
          if (cb_autorecord.isSelected())
             h.put("autorecord", "yes");
          return h;
@@ -105,5 +163,96 @@ public class wlOptions {
       tf_title_keyword.setText("");
       tf_actor.setText("");
       tf_director.setText("");
+   }
+   
+   // Build list of top level categories plus sub-categories which can be used
+   // for Wishlist creation
+   private JSONArray getWishlistCategoryIds(String tivoName) {
+      Remote r = new Remote(tivoName, true);
+      if (r.success) {
+         try {
+            JSONObject json = new JSONObject();
+            // 1st get top level categories and filter out those with partnerId
+            json.put("orderBy", "label");
+            json.put("topLevelOnly", true);
+            json.put("noLimit", true);
+            JSONObject result = r.Command("categorySearch", json);
+            if (result != null && result.has("category")) {
+               JSONArray top = result.getJSONArray("category");
+               for (int i=0; i<top.length(); ++i) {
+                  if (top.getJSONObject(i).has("partnerId"))
+                     top.remove(i);
+               }
+               
+               // Build initial return JSONArray with top level info
+               JSONArray categories = new JSONArray();
+               for (int i=0; i<top.length(); ++i) {
+                  JSONObject j = new JSONObject();
+                  j.put("name", top.getJSONObject(i).getString("label"));
+                  j.put("categoryId", top.getJSONObject(i).getString("categoryId"));
+                  categories.put(j);
+               }
+               
+               // Now add in sub-categories
+               for (int i=0; i<categories.length(); ++i) {
+                  JSONObject j = new JSONObject();
+                  j.put("orderBy", "label");
+                  j.put("noLimit", true);
+                  j.put("parentCategoryId", categories.getJSONObject(i).get("categoryId"));
+                  result = r.Command("categorySearch", j);
+                  if (result != null && result.has("category")) {
+                     JSONArray a = result.getJSONArray("category");
+                     JSONArray sa = new JSONArray();
+                     for (int k=0; k<a.length(); ++k) {
+                        JSONObject sj = new JSONObject();
+                        sj.put("name", a.getJSONObject(k).getString("label"));
+                        sj.put("categoryId", a.getJSONObject(k).getString("categoryId"));
+                        sa.put(sj);
+                     }
+                     categories.getJSONObject(i).put("subcategories", sa);
+                  }
+               }               
+               return categories;
+            }
+         } catch (JSONException e) {
+            log.error("getWishlistCategoryIds - " + e.getMessage());
+            return null;
+         }
+      }
+      return null;
+   }
+   
+   private String findCategoryId(String name) {
+      String main, sub=null;
+      if (name.contains(":")) {
+         String[] s = name.split(":");
+         main = s[0];
+         sub = s[1];
+         main = string.removeLeadingTrailingSpaces(main);
+         sub = string.removeLeadingTrailingSpaces(sub);
+      } else {
+         main = name;
+      }
+      try {
+         for (int i=0; i<wishlistCategories.length(); ++i) {
+            JSONObject json = wishlistCategories.getJSONObject(i);
+            if (json.getString("name").equals(main)) {
+               if (sub == null)
+                  return json.getString("categoryId");
+               else {
+                  JSONArray a = json.getJSONArray("subcategories");
+                  for (int j=0; j<a.length(); ++j) {
+                     if (a.getJSONObject(j).getString("name").equals(sub)) {
+                        log.print("name=" + name + " categoryId=" + a.getJSONObject(j).getString("categoryId"));
+                        return a.getJSONObject(j).getString("categoryId");
+                     }
+                  }
+               }
+            }
+         }
+      } catch (JSONException e) {
+         log.error("findCategoryId - " + e.getMessage());
+      }
+      return null;
    }
 }
