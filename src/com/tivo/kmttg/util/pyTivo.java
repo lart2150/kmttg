d8b237c8e8b06b74e1a3a71dd10a2597033de23e
moyekj@yahoo.com <moyekj@yahoo.com@b0df3fdd-2057-4f73-b226-dfc9190e8f7a>
2013-01-01 22:25:24 +0000

    Added streamFile method for option to use TiVoCast HME for streaming to TiVo using pyTivo as server
    git-svn-id: https://svn.code.sf.net/p/kmttg/code/trunk@2223 b0df3fdd-2057-4f73-b226-dfc9190e8f7a

--- src/com/tivo/kmttg/util/pyTivo.java
+++ src/com/tivo/kmttg/util/pyTivo.java
@@ -3,10 +3,20 @@ package com.tivo.kmttg.util;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.net.UnknownHostException;
 import java.util.Hashtable;
 import java.util.Stack;
 
+import com.tivo.kmttg.JSON.JSONArray;
+import com.tivo.kmttg.JSON.JSONObject;
+import com.tivo.kmttg.JSON.XML;
 import com.tivo.kmttg.main.config;
+import com.tivo.kmttg.rpc.Remote;
 
 public class pyTivo {
    
@@ -136,6 +146,178 @@ public class pyTivo {
       }
       return lowercased;
    }
+      
+   private static Hashtable<String,String> getShareInfo(String videoFile, Stack<Hashtable<String,String>> shares) {
+      if (shares == null) {
+         return null;
+      }
+      if (shares.size() == 0) {
+         return null;
+      }
+      for (int i=0; i<shares.size(); ++i) {
+         if (videoFile.startsWith(shares.get(i).get("path"))) {
+            String shareDir = shares.get(i).get("path");
+            String share = shares.get(i).get("share");
+            String path = string.dirname(videoFile.substring(shareDir.length()+1, videoFile.length()));
+            if (config.OS.equals("windows")) {
+               path = path.replaceAll("\\\\", "/");
+            }
+            if (path.endsWith("/")) {
+               path = path.substring(0,path.length()-1);
+            }
+            Hashtable<String,String> h = new Hashtable<String,String>();
+            h.put("share", share);
+            h.put("path", path);
+            return h;
+         }
+      }
+      return null;
+   }
+  
+   public static Boolean streamFile(String tivoName, String videoFile) {
+      videoFile = pyTivo.lowerCaseVolume(videoFile);
+      
+      Stack<Hashtable<String,String>> shares = pyTivo.parsePyTivoConf(config.pyTivo_config);      
+      if (shares == null) {
+         log.error("No pyTivo video shares found in pyTivo config file: " + config.pyTivo_config);
+         return false;
+      }
+      if (shares.size() == 0) {
+         log.error("No pyTivo video shares found in pyTivo config file: " + config.pyTivo_config);
+         return false;
+      }
+      // Check that file to be pushed resides under a pyTivo share
+      Hashtable<String,String> h = getShareInfo(videoFile, shares);
+      if ( h == null ) {
+         log.error("This file is not located in a pyTivo share directory");
+         log.error("Available pyTivo shares:");
+         log.error(shares.toString());
+         return false;
+      }
+      String share = urlEncode(h.get("share"));
+      String path = urlEncode(h.get("path"));
+      if (! path.startsWith("/"))
+         path = "/" + path;
+      String file = urlEncode(string.basename(videoFile));
+      String shareString = "/" + share + path + "/" + file;
 
+      String host = config.pyTivo_host;
+      if (host.equals("localhost")) {
+         try {
+            InetAddress localhost = InetAddress.getLocalHost();
+            host = localhost.getHostAddress();
+         } catch (UnknownHostException e) {
+            log.error("streamFile - " + e.getMessage());
+         }
+      }
+      String mime = "video/x-tivo-mpeg";
+      if (file.endsWith(".mp4"))
+         mime = "video/mp4";
+      JSONObject videoInfo = getVideoDetails(host, config.pyTivo_port, share, path, file);
+      if (videoInfo == null) {
+         log.error("Could not determine video information from pyTivo");
+         return false;
+      }
+      
+      try {
+         String title = file;
+         String subtitle = "";
+         String description = "";
+         int duration = 0;
+         if (videoInfo.has("program")) {
+            JSONObject program = videoInfo.getJSONObject("program");
+            if (program.has("title"))
+               title = program.getString("title");
+            if (program.has("subtitle"))
+               subtitle = program.getString("subtitle");
+            if (program.has("description"))
+               description = program.getString("description");
+            if (program.has("vHost")) {
+               JSONObject vHost = program.getJSONObject("vHost");
+               if (vHost.has("element")) {
+                  JSONArray element = vHost.getJSONArray("element");
+                  for (int i=0; i<element.length(); ++i) {
+                     String s = element.getString(i);
+                     if (s.contains("millisecs")) {
+                        s = s.replaceFirst("millisecs=", "");
+                        duration = Integer.parseInt(s)/1000;
+                     }
+                     if (s.contains("vCodec")) {
+                        if (s.contains("h264"))
+                           mime = "video/mp4";
+                     }
+                  }
+               }
+            }
+         }
+         String urlString = "http://" + host + ":" + config.pyTivo_port;
+         urlString += shareString;
+         urlString += "?Format=" + urlEncode(mime);
+         log.print("using pyTivo url: " + urlString);
+         
+         Remote r = new Remote(tivoName);
+         if (r.success) {
+            JSONObject param = new JSONObject();
+            param.put("uri", urlString);
+            param.put("title", title);
+            param.put("subtitle", subtitle);
+            param.put("description", description);
+            if (duration > 0)
+               param.put("duration", duration);
+            JSONObject json = new JSONObject();
+            json.put("uiDestinationType", "hme");
+            json.put("uri", "x-tivo:hme:uuid:863cb78f-efdd-4106-b572-51733983dc76");
+            json.put("parameters", param);
+            JSONObject result = r.Command("uiNavigate", json);
+            if (result != null)
+               log.print("Initiated stream of file:" + videoFile);
+         }
+      } catch (Exception e) {
+         log.error("streamFile - " + e.getMessage());
+         return false;
+      }
+      return true;
+   }
+   
+   private static String urlEncode(String s) {
+      String encoded;
+      try {
+         encoded = URLEncoder.encode(s, "UTF-8");
+         return encoded;
+      } catch (Exception e) {
+         log.error("Cannot encode url: " + s);
+         log.error(e.toString());
+         return s;
+      }
+   }
+   
+   // Get details on a specific video
+   public static JSONObject getVideoDetails(String pyTivo_host, String pyTivo_port, String share, String path, String file) {
+      String urlString = "http://" + pyTivo_host + ":" + pyTivo_port +
+      "/TiVoConnect?Command=TVBusQuery&Container=";
+      urlString += share + "&File=" + path + "/" + file;
+      try {
+         URL url = new URL(urlString);
+         URLConnection con = url.openConnection();
+         BufferedReader in = new BufferedReader( new InputStreamReader(con.getInputStream()));
+         String line;
+         String xml = "";
+         while ((line = in.readLine()) != null) {
+            xml += line + "\n";
+         }
+         if (xml.length() > 0) {
+            JSONObject result = XML.toJSONObject(xml);
+            if (result != null && result.has("TvBusMarshalledStruct:TvBusEnvelope")) {
+               JSONObject json = result.getJSONObject("TvBusMarshalledStruct:TvBusEnvelope");
+               if (json.has("showing"))
+                  return json.getJSONObject("showing");
+            }
+         }
+      } catch (Exception e) {
+         log.error("getVideoDetails - " + e.getMessage());
+      }
+
+      return null;
+   }
 
 }
