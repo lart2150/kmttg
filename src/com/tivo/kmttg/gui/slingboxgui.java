74a983cae72ec3d2dcd16d8f4a3460db7e09799c
moyekj <moyekj@b0df3fdd-2057-4f73-b226-dfc9190e8f7a>
2013-06-16 06:22:26 +0000

    New slingbox capture capability
    git-svn-id: https://svn.code.sf.net/p/kmttg/code/trunk@2575 b0df3fdd-2057-4f73-b226-dfc9190e8f7a

---
+++ src/com/tivo/kmttg/gui/slingboxgui.java
@@ -0,0 +1,325 @@
+package com.tivo.kmttg.gui;
+
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import com.tivo.kmttg.main.config;
+import com.tivo.kmttg.main.jobData;
+import com.tivo.kmttg.main.jobMonitor;
+import com.tivo.kmttg.util.log;
+import com.tivo.kmttg.util.string;
+
+public class slingboxgui {
+   private JPanel panel = null;
+   private JTextField dir;
+   private JTextField perl;
+   private JTextField ip;
+   private JTextField port;
+   private JTextField pass;
+   private JComboBox vbw;
+   private JComboBox res;
+   jobData job = null;
+   JFileChooser Browser = null;
+   
+   slingboxgui(JFrame frame) {
+      getPanel();
+      Browser = new JFileChooser(config.programDir);
+      Browser.setMultiSelectionEnabled(false);
+   }
+   
+   public JPanel getPanel() {
+      if (panel == null) {
+         panel = new JPanel(new GridBagLayout());
+         
+         JButton start = new JButton("Start");
+         start.setToolTipText(getToolTip("start"));
+         start.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent e) {
+               String slingbox_file = getFileName();
+               if (slingbox_file == null)
+                  return;
+               updateConfig();
+               
+               // Sanity checking
+               Boolean OK = true;
+               if (config.slingBox_dir.length() == 0) {
+                  log.error("Slingbox capture file directory not specified.");
+                  OK = false;
+               }
+               if (config.slingBox_perl.length() == 0) {
+                  log.error("Perl executable not specified.");
+                  OK = false;
+               }
+               if (config.slingBox_ip.length() == 0) {
+                  log.error("Slingbox IP not specified.");
+                  OK = false;
+               }
+               if (config.slingBox_port.length() == 0) {
+                  log.error("Slingbox port not specified.");
+                  OK = false;
+               }
+               if (config.slingBox_pass.length() == 0) {
+                  log.error("Slingbox password not specified.");
+                  OK = false;
+               }
+               
+               // Proceed
+               if (OK) {
+                  job = new jobData();
+                  job.source        = "slingbox";
+                  job.type          = "slingbox";
+                  job.name          = "Slingbox";
+                  job.tivoName      = "Slingbox";
+                  job.slingbox_perl = perl.getText();
+                  job.slingbox_file = slingbox_file;
+                  jobMonitor.submitNewJob(job);
+               }
+            }
+         });
+         
+         JButton stop = new JButton("Stop");
+         stop.setToolTipText(getToolTip("stop"));
+         stop.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent e) {
+               if (job != null) {
+                  jobMonitor.kill(job);
+                  job = null;
+               }
+            }
+         });
+         
+         JLabel dir_label = new JLabel("Slingbox capture file directory");
+         dir = new JTextField(30);
+         dir.setToolTipText(getToolTip("dir"));
+         dir.setText(config.slingBox_dir);
+         dir.addMouseListener(
+            new MouseAdapter() {
+               public void mouseClicked(MouseEvent e) {
+                  if(e.getClickCount() == 2) {
+                     Browser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                     int result = Browser.showDialog(dir, "Choose Directory");
+                     if (result == JFileChooser.APPROVE_OPTION) {
+                        dir.setText(Browser.getSelectedFile().getPath());
+                     }
+                  }
+               }
+            }
+         );
+         
+         JLabel perl_label = new JLabel("Perl executable");
+         perl = new JTextField(30);
+         perl.setToolTipText(getToolTip("perl"));
+         perl.setText(config.slingBox_perl);
+         perl.addMouseListener(
+            new MouseAdapter() {
+               public void mouseClicked(MouseEvent e) {
+                  if(e.getClickCount() == 2) {
+                     Browser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+                     int result = Browser.showDialog(perl, "Choose File");
+                     if (result == JFileChooser.APPROVE_OPTION) {
+                        perl.setText(Browser.getSelectedFile().getPath());
+                     }
+                  }
+               }
+            }
+         );
+         
+         JLabel pass_label = new JLabel("Slingbox password");
+         pass = new JTextField(30);
+         pass.setToolTipText(getToolTip("pass"));
+         pass.setText(config.slingBox_pass);
+         
+         JLabel ip_label = new JLabel("Slingbox IP");
+         ip = new JTextField(30);
+         ip.setToolTipText(getToolTip("ip"));
+         ip.setText(config.slingBox_ip);
+         
+         JLabel port_label = new JLabel("Slingbox port");
+         port = new JTextField(30);
+         port.setToolTipText(getToolTip("ip"));
+         port.setText(config.slingBox_port);
+         
+         JLabel res_label = new JLabel("Video resolution");
+         res = new JComboBox();
+         res.setToolTipText(getToolTip("res"));
+         String[] r = {"1920x1080", "640x480"};
+         for (int i=0; i<r.length; ++i)
+            res.addItem(r[i]);
+         res.setSelectedItem(config.slingBox_res);
+         
+         JLabel vbw_label = new JLabel("Video bit rate (Kbps)");
+         vbw = new JComboBox();
+         vbw.setToolTipText(getToolTip("vbw"));
+         String[] vb = {"4000", "5000", "6000", "7000"};
+         for (int i=0; i<vb.length; ++i)
+            vbw.addItem(vb[i]);
+         vbw.setSelectedItem(config.slingBox_vbw);
+         
+         int gy = 0;
+         GridBagConstraints c = new GridBagConstraints();
+         c.ipady = 0;
+         c.weighty = 0.0;  // default to no vertical stretch
+         c.weightx = 0.0;  // default to no horizontal stretch
+         c.gridx = 0;
+         c.gridy = gy;
+         c.gridwidth = 1;
+         c.gridheight = 1;
+         c.anchor = GridBagConstraints.CENTER;
+         c.fill = GridBagConstraints.HORIZONTAL;
+                  
+         Dimension space = new Dimension(10,0);
+         c.gridy = gy;
+         JPanel row0 = new JPanel();
+         row0.setLayout(new BoxLayout(row0, BoxLayout.LINE_AXIS));
+         row0.add(start);
+         row0.add(Box.createRigidArea(space));
+         row0.add(stop);
+         panel.add(row0, c);
+         
+         gy++;
+         c.gridy = gy;
+         c.gridx = 0;
+         panel.add(ip_label, c);
+         c.gridx = 1;
+         panel.add(ip, c);
+         
+         gy++;
+         c.gridy = gy;
+         c.gridx = 0;
+         panel.add(port_label, c);
+         c.gridx = 1;
+         panel.add(port, c);
+         
+         gy++;
+         c.gridy = gy;
+         c.gridx = 0;
+         panel.add(pass_label, c);
+         c.gridx = 1;
+         panel.add(pass, c);
+         
+         gy++;
+         c.gridy = gy;
+         c.gridx = 0;
+         panel.add(dir_label, c);
+         c.gridx = 1;
+         panel.add(dir, c);
+         
+         gy++;
+         c.gridy = gy;
+         c.gridx = 0;
+         panel.add(perl_label, c);
+         c.gridx = 1;
+         panel.add(perl, c);
+         
+         gy++;
+         c.gridy = gy;
+         c.gridx = 0;
+         JPanel left_video = new JPanel();
+         left_video.setLayout(new BoxLayout(left_video, BoxLayout.LINE_AXIS));
+         left_video.add(res_label);
+         left_video.add(Box.createRigidArea(space));
+         left_video.add(res);
+         panel.add(left_video, c);
+         c.gridx = 1;
+         JPanel right_video = new JPanel();
+         right_video.setLayout(new BoxLayout(right_video, BoxLayout.LINE_AXIS));
+         right_video.add(Box.createRigidArea(space));
+         right_video.add(vbw_label);
+         right_video.add(Box.createRigidArea(space));
+         right_video.add(vbw);
+         panel.add(right_video, c);
+      }
+      return panel;
+   }
+   
+   public void updateConfig() {
+      config.slingBox_dir = string.removeLeadingTrailingSpaces(dir.getText());
+      config.slingBox_perl = string.removeLeadingTrailingSpaces(perl.getText());
+      config.slingBox_pass = string.removeLeadingTrailingSpaces(pass.getText());
+      config.slingBox_ip = string.removeLeadingTrailingSpaces(ip.getText());
+      config.slingBox_port = string.removeLeadingTrailingSpaces(port.getText());
+      config.slingBox_vbw = (String)vbw.getSelectedItem();
+      config.slingBox_res = (String)res.getSelectedItem();
+   }
+   
+   private String getTimeStamp() {
+      long now = new Date().getTime();
+      SimpleDateFormat sdf = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss");
+      return sdf.format(now);
+   }
+   
+   private String getFileName() {
+      String d = string.removeLeadingTrailingSpaces(dir.getText());
+      if (d.length() == 0) {
+         log.error("No slingbox directory specified. Aborting...");
+         return null;
+      }
+      return d + File.separator + "slingbox_" + getTimeStamp() + ".ts";
+   }
+   
+   private String getToolTip(String component) {
+      String text = "";
+      if (component.equals("start")){
+         text = "<b>Start</b><br>";
+         text += "Start slingbox capture. The capture will continue until you press <b>Stop</b>.<br>";
+         text += "A time stamped slingbox capture file will be created in the specified directory.<br>";
+         text += "NOTE: This requires Perl with proper module. Read the kmttg <b>slingbox_capture</b> Wiki.";
+      } 
+      else if (component.equals("stop")) {
+         text = "<b>Stop</b><br>";
+         text += "Stop slingbox capture.";
+      }
+      else if (component.equals("ip")) {
+         text = "<b>Slingbox IP or port</b><br>";
+         text += "Slingbox IP or port. To find Slingbox IP & port:<br>";
+         text += "While streaming Slingbox stream to browser, click on <b>Settings</b>, then<br>";
+         text += "click on <b>NETWORK DETAILS</b> where you can see HOME IP Address and Network Port.";
+      }
+      else if (component.equals("pass")) {
+         text = "<b>Slingbox password</b><br>";
+         text += "Slingbox admin password. To find this password:<br>";
+         text += "While streaming Slingbox stream to browser, visit following URL:<br>";
+         text += "https://newwatchsecure.slingbox.com/watch/slingAccounts/account_boxes_js<br>";
+         text += "Then look for <b>adminPassword</b> in the .js file.";
+      }
+      else if (component.equals("dir")) {
+         text = "<b>Slingbox capture file directory</b><br>";
+         text += "Directory in which to save Slingbox capture files.<br>";
+         text += "NOTE: Double-click in this field to bring up file browser.";
+      }
+      else if (component.equals("perl")) {
+         text = "<b>Perl executable</b><br>";
+         text += "Full path to the Perl executable.<br>";
+         text += "NOTE: Make sure to add Crypt::Tea_JS module via Perl Package Manager.<br>";
+         text += "NOTE: Double-click in this field to bring up file browser.";
+      }
+      else if (component.equals("res")) {
+         text = "<b>Video resolution</b><br>";
+         text += "Video resolution to use for the capture.";
+      }
+      else if (component.equals("vbw")) {
+         text = "<b>Video bit rate (Kbps)</b><br>";
+         text += "Video bit rate in Kbps to use for the capture.";
+      }
+      if (text.length() > 0) {
+         text = "<html>" + text + "</html>";
+      }
+      return text;
+   }
+}
---
+++ src/com/tivo/kmttg/task/slingbox.java
@@ -0,0 +1,295 @@
+package com.tivo.kmttg.task;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Stack;
+import java.util.UUID;
+
+import com.tivo.kmttg.main.config;
+import com.tivo.kmttg.main.jobData;
+import com.tivo.kmttg.main.jobMonitor;
+import com.tivo.kmttg.util.backgroundProcess;
+import com.tivo.kmttg.util.debug;
+import com.tivo.kmttg.util.file;
+import com.tivo.kmttg.util.log;
+
+public class slingbox implements Serializable {
+   private static final long serialVersionUID = 1L;
+   String command = "";
+   String script = "";
+   String pidFile = "";
+   String uniqueName = "";
+   String perl_script;
+   private backgroundProcess process;
+   public jobData job;
+   
+   public slingbox(jobData job) {
+      debug.print("job=" + job);
+      this.job = job;
+            
+      // Generate unique script names
+      script = file.makeTempFile("script");
+      pidFile = file.makeTempFile("pid");
+      uniqueName = UUID.randomUUID().toString();
+      if (config.OS.equals("windows"))
+         script += ".bat";
+   }
+   
+   public backgroundProcess getProcess() {
+      return process;
+   }
+   
+   public Boolean launchJob() {
+      debug.print("");
+      Boolean schedule = true;
+      
+      perl_script = config.programDir + File.separator + "slingbox" + File.separator + "rec350.pl";
+      if ( ! file.isFile(perl_script) ) {
+         log.error("Can't find perl capture script: " + perl_script);
+         schedule = false;
+      }
+      
+      if ( ! file.isFile(job.slingbox_perl) ) {
+         log.error("Invalid Perl executable: " + job.slingbox_perl);
+         schedule = false;
+      }
+      
+      if ( ! file.isFile(config.ffmpeg) ) {             
+         log.error("ffmpeg not found: " + config.ffmpeg);
+         schedule = false;
+      }
+      
+      if (config.slingBox_ip.length() == 0) {
+         log.error("Slingbox IP not specified");
+         schedule = false;
+      }
+      
+      if (config.slingBox_port.length() == 0) {
+         log.error("Slingbox port not specified");
+         schedule = false;
+      }
+      
+      if (config.slingBox_pass.length() == 0) {
+         log.error("Slingbox password not specified");
+         schedule = false;
+      }
+      
+      if (schedule) {
+         // Create sub-folders for output file if needed
+         if ( ! jobMonitor.createSubFolders(job.slingbox_file, job) ) schedule = false;
+      }
+      if (file.isFile(job.slingbox_file))
+            file.delete(job.slingbox_file);
+      
+      if (schedule) {
+         if ( start() ) {
+            job.process_slingbox = this;
+            jobMonitor.updateJobStatus(job, "running");
+            job.time             = new Date().getTime();
+         }
+         return true;
+      } else {
+         return false;
+      }      
+   }
+   
+   private Boolean start() {
+      debug.print("");
+      String vs = "16";
+      if (config.slingBox_res.equals("1920x1080"))
+         vs = "16";
+      if (config.slingBox_res.equals("640x480"))
+         vs = "5";
+      // Make main piped command string
+      command = "\"" + job.slingbox_perl + "\" \"" + perl_script + "\" " +
+         "-stdout " +
+         "-ip "     + config.slingBox_ip + " " +
+         "-port "   + config.slingBox_port + " " +
+         "-pass "   + config.slingBox_pass + " " +
+         "-vbw "    + config.slingBox_vbw + " " +
+         "-vs "     + vs;
+      command += " | \"" + config.ffmpeg + "\" -fflags +genpts -i - ";
+      command += "-vcodec copy -acodec ac3 -ab 224k -y -f mpegts \"" + job.slingbox_file + "\"";
+      
+      // Make temporary script containing command
+      try {
+         BufferedWriter ofp = new BufferedWriter(new FileWriter(script));
+         if (config.OS.equals("windows")) {
+            hackToGetPid(ofp, uniqueName, pidFile);
+         }
+         ofp.write(command);
+         if (config.OS.equals("windows"))
+            ofp.write("\r");
+         ofp.write("\n");
+         ofp.close();
+      } catch (IOException e) {
+         log.error(e.toString());
+         return false;
+      }
+
+      // Execute above script in native OS shell
+      Stack<String> c = new Stack<String>();      
+      if (config.OS.equals("windows")) {
+         c.add("cmd.exe");
+         c.add("/c");
+      } else {
+         c.add("sh");
+      }
+      c.add(script);
+      process = new backgroundProcess();
+      String message = "CAPTURING SLINGBOX";
+      log.print(">> " + message + " TO " + job.slingbox_file + " ...");
+      if ( process.run(c) ) {
+         log.print(command);
+      } else {
+         log.error("Failed to start command: " + command);
+         process.printStderr();
+         process = null;
+         jobMonitor.removeFromJobList(job);
+         return false;
+      }
+      return true;
+   }
+   
+   public void kill() {
+      debug.print("");
+      log.warn("Killing '" + job.type + "' job: " + command);
+      if (config.OS.equals("windows")) {
+         // For Windows process.kill doesn't kill child processes so this is a hack to do that
+         try {
+            String pid = getPidFromFile();
+            if (pid != null) {
+               log.warn("killing windows pid=" + pid);
+               Process p = Runtime.getRuntime().exec("taskkill /f /t /pid " + pid);
+               p.waitFor();
+            }
+         } catch (Exception e) {
+            log.error("Exception finding/killing pid: " + e.getMessage());
+         }
+      }
+      process.kill();
+      cleanup();
+   }
+   
+   // Check status of a currently running job
+   // Returns true if still running, false if job finished
+   // If job is finished then check result
+   public Boolean check() {
+      //debug.print("");
+      int exit_code = process.exitStatus();
+      if (exit_code == -1) {
+         // Still running
+         if (config.GUIMODE && file.isFile(job.slingbox_file)) {
+            // Update status in job table
+            Long size = file.size(job.slingbox_file);
+            String s = String.format("%.2f MB", (float)size/Math.pow(2,20));
+            String t = jobMonitor.getElapsedTime(job.time);
+            
+            if ( jobMonitor.isFirstJobInMonitor(job) ) {
+               // Update STATUS column 
+               config.gui.jobTab_UpdateJobMonitorRowStatus(job, t + "---" + s);
+               
+               // If 1st job then update title
+               String title = String.format("slingbox %s %s", t, config.kmttg);
+               config.gui.setTitle(title);
+            } else {
+               // Update STATUS column            
+               config.gui.jobTab_UpdateJobMonitorRowStatus(job, t + "---" + s);
+            }
+         }
+         return true;
+      } else {
+         // Job finished
+         if (config.GUIMODE) {
+            if ( jobMonitor.isFirstJobInMonitor(job) ) {
+               config.gui.setTitle(config.kmttg);
+            }
+         }
+         
+         jobMonitor.removeFromJobList(job);
+         
+         // Check for problems
+         int failed = 0;
+         
+         // exit code != 0 => trouble
+         if (exit_code != 0) {
+            failed = 1;
+         }
+         
+         // No or empty output means problems         
+         if ( file.isEmpty(job.slingbox_file) ) {
+            failed = 1;
+         } else {
+            // Print statistics for the job
+            String s = String.format("%.2f MB", file.size(job.slingbox_file)/Math.pow(2,20));
+            String t = jobMonitor.getElapsedTime(job.time);
+            log.warn(job.mpegFile + ": size=" + s + " elapsed=" + t);
+         }
+         
+         if (failed == 1) {
+            log.error("Capture to file failed: " + job.slingbox_file);
+            log.error("Exit code: " + exit_code);
+            process.printStderr();
+            file.delete(job.slingbox_file);
+         } else {
+            log.print("---DONE--- job=" + job.type + " output=" + job.slingbox_file);
+         }
+      } // job finished
+      cleanup();
+      
+      return false;
+   }
+   
+   // Hack lines to add to Windows script file to obtain pid
+   // This is needed to be able to kill cmd.exe using taskkill
+   private void hackToGetPid(BufferedWriter ofp, String uniqueName, String pidFile) {
+      try {
+         String eol = "\r\n";
+         ofp.write("@echo off" + eol);
+         ofp.write("set name=" + uniqueName + eol);
+         ofp.write("TITLE %name%" + eol);
+         ofp.write("TASKLIST /V /NH | findstr /i \"%name%\" > \"" + pidFile + "\"" + eol);
+      } catch (IOException e) {
+         log.error(e.toString());
+      }
+   }
+  
+   // Get pid from file with single line:
+   // Sample line looks like (pid is the 2nd column):
+   // cmd.exe 2800 RDP-Tcp#1 0 2,988 K Running INET\moyekj 0:00:00 ...
+   private String getPidFromFile() {
+      if (file.isFile(pidFile)) {
+         try {
+            BufferedReader ifp = new BufferedReader(new FileReader(pidFile));
+            String line = ifp.readLine();
+            ifp.close();
+            String pid = "";
+            String s[] = line.split("\\s+");
+            if (s.length > 2)
+               pid = s[1];
+            if (pid.length() > 0 && pid.matches("^\\d+\\s*")) {
+               return pid;
+            } else {
+               log.error("Unable to determine windows pid to kill windows process");
+               log.error("(pidFile line is: '" + line + "')");
+            }
+         }
+         catch (IOException ex) {
+            log.error("getPidFromFile error: " + ex.toString());
+            return null;
+         }
+      }
+      return null;
+   }
+   
+   private void cleanup() {
+      file.delete(script);
+      file.delete(pidFile);
+   }
+}
