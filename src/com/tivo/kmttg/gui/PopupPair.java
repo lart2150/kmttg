d53ebba9b079e8014ce5a6fdc0f0ede9572b4b9d
moyekj <moyekj@b0df3fdd-2057-4f73-b226-dfc9190e8f7a>
2013-07-04 22:40:54 +0000

    Class to aid in popup menu item creation
    git-svn-id: https://svn.code.sf.net/p/kmttg/code/trunk@2632 b0df3fdd-2057-4f73-b226-dfc9190e8f7a

---
+++ src/com/tivo/kmttg/gui/PopupPair.java
@@ -0,0 +1,11 @@
+package com.tivo.kmttg.gui;
+
+public class PopupPair {
+   public String name;
+   public int key;
+   
+   PopupPair(String name, int key) {
+      this.name = name;
+      this.key = key;
+   }
+}
