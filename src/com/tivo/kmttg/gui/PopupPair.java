7ce2482ad355927dde989a162a153addbd716275
moyekj <moyekj@b0df3fdd-2057-4f73-b226-dfc9190e8f7a>
2013-07-14 14:59:37 +0000

    Added tableName
    git-svn-id: https://svn.code.sf.net/p/kmttg/code/trunk@2661 b0df3fdd-2057-4f73-b226-dfc9190e8f7a

--- src/com/tivo/kmttg/gui/PopupPair.java
+++ src/com/tivo/kmttg/gui/PopupPair.java
@@ -3,9 +3,11 @@ package com.tivo.kmttg.gui;
 public class PopupPair {
    public String name;
    public int key;
+   public String tableName;
    
-   PopupPair(String name, int key) {
+   PopupPair(String name, int key, String tableName) {
       this.name = name;
       this.key = key;
+      this.tableName = tableName;
    }
 }
