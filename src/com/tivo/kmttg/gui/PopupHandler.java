314d65ac92b53873a7cc31f5e660c672baa8d9ad
moyekj <moyekj@b0df3fdd-2057-4f73-b226-dfc9190e8f7a>
2013-07-14 15:01:01 +0000

    Added Season Passes table support
    git-svn-id: https://svn.code.sf.net/p/kmttg/code/trunk@2663 b0df3fdd-2057-4f73-b226-dfc9190e8f7a

--- src/com/tivo/kmttg/gui/PopupHandler.java
+++ src/com/tivo/kmttg/gui/PopupHandler.java
@@ -25,37 +25,54 @@ public class PopupHandler {
          // This is a Remote table
          String subTabName = config.gui.remote_gui.getCurrentTabName();
          tivoName = config.gui.remote_gui.getTivoName(subTabName);
-         if (config.rpcEnabled(tivoName))
-            items.add(new PopupPair("Show Information [i]", KeyEvent.VK_I));
-         items.add(new PopupPair("Display data [j]", KeyEvent.VK_J));
-         items.add(new PopupPair("Web query [q]", KeyEvent.VK_Q));
+         if (subTabName.equals("Season Passes")) {
+            items.add(new PopupPair("Delete [delete]", KeyEvent.VK_DELETE, subTabName));
+            items.add(new PopupPair("Copy [c]", KeyEvent.VK_C, subTabName));
+            items.add(new PopupPair("Modify [m]", KeyEvent.VK_M, subTabName));
+            items.add(new PopupPair("Upcoming [u]", KeyEvent.VK_U, subTabName));
+            items.add(new PopupPair("Conflicts [o]", KeyEvent.VK_O, subTabName));
+         }
+         if (config.rpcEnabled(tivoName) && !subTabName.equals("Season Passes"))
+            items.add(new PopupPair("Show Information [i]", KeyEvent.VK_I, subTabName));
+         items.add(new PopupPair("Display data [j]", KeyEvent.VK_J, subTabName));
+         items.add(new PopupPair("Web query [q]", KeyEvent.VK_Q, subTabName));
       } else {
          // This is a NPL table
          tivoName = tabName;
          if (!config.rpcEnabled(tivoName) && !config.mindEnabled(tivoName))
-            items.add(new PopupPair("Get extended metadata [m]", KeyEvent.VK_M));
+            items.add(new PopupPair("Get extended metadata [m]", KeyEvent.VK_M, tivoName));
          if (config.rpcEnabled(tivoName) || config.twpDeleteEnabled())
-            items.add(new PopupPair("Delete [delete]", KeyEvent.VK_DELETE));
+            items.add(new PopupPair("Delete [delete]", KeyEvent.VK_DELETE, tivoName));
          if (config.rpcEnabled(tivoName)) {
-            items.add(new PopupPair("Play [space]", KeyEvent.VK_SPACE));
-            items.add(new PopupPair("Show Information [i]", KeyEvent.VK_I));
+            items.add(new PopupPair("Play [space]", KeyEvent.VK_SPACE, tivoName));
+            items.add(new PopupPair("Show Information [i]", KeyEvent.VK_I, tivoName));
          }
-         items.add(new PopupPair("Display data [j]", KeyEvent.VK_J));
-         items.add(new PopupPair("Web query [q]", KeyEvent.VK_Q));
+         items.add(new PopupPair("Display data [j]", KeyEvent.VK_J, tivoName));
+         items.add(new PopupPair("Web query [q]", KeyEvent.VK_Q, tivoName));
       }
       
       for (int i=0; i<items.size(); ++i) {
          final int key = items.get(i).key;
          JMenuItem item = new JMenuItem(items.get(i).name);
+         final String tableName = items.get(i).tableName;
          item.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                // Dispatch key event
-               TABLE.dispatchEvent(
-                  new KeyEvent(
-                     TABLE, KeyEvent.KEY_RELEASED, System.currentTimeMillis(), 0,
-                     key, KeyEvent.CHAR_UNDEFINED
-                  )
-               );                
+               if (tableName.equals("Season Passes")) {
+                  TABLE.dispatchEvent(
+                        new KeyEvent(
+                           TABLE, KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0,
+                           key, KeyEvent.CHAR_UNDEFINED
+                        )
+                     );                  
+               } else {
+                  TABLE.dispatchEvent(
+                     new KeyEvent(
+                        TABLE, KeyEvent.KEY_RELEASED, System.currentTimeMillis(), 0,
+                        key, KeyEvent.CHAR_UNDEFINED
+                     )
+                  );
+               }
             }
          });  
          popup.add(item);
