d101975c3757467416d501bef5f0396bfda36f95
moyekj <moyekj@b0df3fdd-2057-4f73-b226-dfc9190e8f7a>
2013-07-28 21:37:36 +0000

    Added new menuitem entry & associated constructor
    git-svn-id: https://svn.code.sf.net/p/kmttg/code/trunk@2693 b0df3fdd-2057-4f73-b226-dfc9190e8f7a

--- src/com/tivo/kmttg/gui/PopupPair.java
+++ src/com/tivo/kmttg/gui/PopupPair.java
@@ -1,13 +1,22 @@
 package com.tivo.kmttg.gui;
 
+import javax.swing.JMenuItem;
+
 public class PopupPair {
    public String name;
    public int key;
    public String tableName;
+   public JMenuItem menuitem = null;
    
    PopupPair(String name, int key, String tableName) {
       this.name = name;
       this.key = key;
+      this.tableName = tableName;
+   }
+   
+   PopupPair(String name, JMenuItem menuitem, String tableName) {
+      this.name = name;
+      this.menuitem = menuitem;
       this.tableName = tableName;
    }
 }
