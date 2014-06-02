0aa1c9797d66eaa6dae8ee0142d9f510b6ca5a44
moyekj <moyekj@b0df3fdd-2057-4f73-b226-dfc9190e8f7a>
2014-06-02 15:14:08 +0000

    Added suffix file renaming if necessary for TS .TiVo files
    git-svn-id: https://svn.code.sf.net/p/kmttg/code/trunk@3007 b0df3fdd-2057-4f73-b226-dfc9190e8f7a

--- src/com/tivo/kmttg/task/dsd.java
+++ src/com/tivo/kmttg/task/dsd.java
@@ -2,6 +2,7 @@ package com.tivo.kmttg.task;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.Hashtable;
 import java.util.Stack;
 
 import com.tivo.kmttg.main.config;
@@ -10,8 +11,11 @@ import com.tivo.kmttg.main.jobMonitor;
 import com.tivo.kmttg.rpc.rnpl;
 import com.tivo.kmttg.util.backgroundProcess;
 import com.tivo.kmttg.util.debug;
+import com.tivo.kmttg.util.ffmpeg;
 import com.tivo.kmttg.util.file;
 import com.tivo.kmttg.util.log;
+import com.tivo.kmttg.util.mediainfo;
+import com.tivo.kmttg.util.string;
 
 public class dsd implements Serializable {
    private static final long serialVersionUID = 1L;
@@ -68,6 +72,55 @@ public class dsd implements Serializable {
    // Return false if starting command fails, true otherwise
    private Boolean start() {
       debug.print("");
+      
+      // Obtain some info on input video
+      Hashtable<String,String> info = null;
+      if (file.isFile(config.mediainfo))
+         info = mediainfo.getVideoInfo(job.tivoFile);
+      else if (file.isFile(config.ffmpeg))
+         info = ffmpeg.getVideoInfo(job.tivoFile);
+       
+      Boolean isFileChanged = false;
+      if (info != null && info.get("container").equals("mpegts")) {
+         if (job.mpegFile.endsWith(".mpg")) {
+            job.mpegFile = string.replaceSuffix(job.mpegFile, ".ts");
+            isFileChanged = true;
+         }
+      } 
+      
+      if (isFileChanged) {            
+         // If in GUI mode, update job monitor output field
+         if (config.GUIMODE) {
+            String output = string.basename(job.mpegFile);
+            if (config.jobMonitorFullPaths == 1)
+               output = job.mpegFile;
+            config.gui.jobTab_UpdateJobMonitorRowOutput(job, output);
+         }
+         
+         // Subsequent jobs need to have mpegFile && mpegFile_cut updated
+         jobMonitor.updatePendingJobFieldValue(job, "mpegFile", job.mpegFile);
+         String mpegFile_cut = job.mpegFile_cut.replaceFirst("_cut.mpg", "_cut.ts");
+         jobMonitor.updatePendingJobFieldValue(job, "mpegFile_cut", mpegFile_cut);
+         
+         // Rename already created metadata file if relevant
+         String meta = string.replaceSuffix(job.mpegFile, ".mpg") + ".txt";
+         if (file.isFile(meta)) {
+            String meta_new = job.mpegFile + ".txt";
+            log.print("Renaming metadata file to: " + meta_new);
+            file.rename(meta, meta_new);
+            // Subsequent jobs need to have metaFile updated
+            jobMonitor.updatePendingJobFieldValue(job, "metaFile", meta_new);
+         }
+         meta = string.replaceSuffix(job.mpegFile_cut, ".mpg") + ".txt";
+         if (file.isFile(meta)) {
+            String meta_new = job.mpegFile_cut + ".txt";
+            log.print("Renaming metadata file to: " + meta_new);
+            file.rename(meta, meta_new);
+            // Subsequent jobs need to have metaFile updated
+            jobMonitor.updatePendingJobFieldValue(job, "metaFile", meta_new);
+         }
+      }
+         
       Stack<String> command = new Stack<String>();
       command.add(config.dsd);
       command.add("-s:" + job.tivoFile);
