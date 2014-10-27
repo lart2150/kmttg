8ee2da02963ee51125b99d81c52c911bf33ed13d
moyekj <moyekj@b0df3fdd-2057-4f73-b226-dfc9190e8f7a>
2014-10-27 01:36:40 +0000

    Added MyShows & tivo download hls transcode
    git-svn-id: https://svn.code.sf.net/p/kmttg/code/trunk@3103 b0df3fdd-2057-4f73-b226-dfc9190e8f7a

--- release/web/js/rpc/Stream.js
+++ release/web/js/rpc/Stream.js
@@ -1,9 +1,56 @@
 $(document).ready(function() {
    // Stream.html document elements
+   TIVO = document.getElementById("TIVO");
    BROWSE = document.getElementById("BROWSE");
    TYPE = document.getElementById("TYPE");
    $("#TYPE").change(function () {FileBrowser();});
+
+   // Retrieve rpc enabled TiVos
+   $.getJSON("/getRpcTivos", function(data) {
+      $.each(data, function( i, value ) {
+         var option = document.createElement("option");
+         option.text = value;
+         option.value = value;
+         TIVO.appendChild(option);
+      });
+   })
+   .error(function(xhr, status) {
+      handleError("/getRpcTivos", xhr, status);
+   });
 });
+
+function MyShows() {
+   var format = $('input[name="type"]:checked').val();
+   var baseUrl = "/transcode?format=" + format + "&url=";
+   $.getJSON("/getMyShows?tivo=" + encodeURIComponent(TIVO.value), function(data) {
+      var html = "";
+      $.each(data, function (i, entry) {
+         if (entry.hasOwnProperty("recording")) {
+            var json = entry.recording[0];
+            //console.log(JSON.stringify(json, null, 3));
+            html += '<a target="_blank" href="' + baseUrl;
+            if (json.hasOwnProperty("__url__")) {
+               url = json.__url__;
+               name = "";
+               if (json.hasOwnProperty("startTime")) {
+                  name = "(" + json.startTime + ") ";
+               }
+               if (json.hasOwnProperty("title")) {
+                  name += json.title;
+               }
+               if (json.hasOwnProperty("subtitle")) {
+                  name += " - " + json.subtitle;
+               }
+               html += encodeURIComponent(url) + '">' + name + '</a><br>';
+            }
+         }
+      });
+      BROWSE.innerHTML = html;
+   })
+   .error(function(xhr, status) {
+      handleError("/getMyShows", xhr, status);
+   });
+}
 
 function FileBrowser() {
    var format = $('input[name="type"]:checked').val();
