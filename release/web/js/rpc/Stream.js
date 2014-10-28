917a9618c27cf5cd93a3fa639aab9a34c3f5ae90
moyekj <moyekj@b0df3fdd-2057-4f73-b226-dfc9190e8f7a>
2014-10-28 00:22:45 +0000

    Use tables
    git-svn-id: https://svn.code.sf.net/p/kmttg/code/trunk@3107 b0df3fdd-2057-4f73-b226-dfc9190e8f7a

--- release/web/js/rpc/Stream.js
+++ release/web/js/rpc/Stream.js
@@ -3,7 +3,9 @@ $(document).ready(function() {
    TIVO = document.getElementById("TIVO");
    BROWSE = document.getElementById("BROWSE");
    TYPE = document.getElementById("TYPE");
+   NPLTABLE = document.getElementById("NPLTABLE");
    $("#TYPE").change(function () {FileBrowser();});
+   NUMCOLS = 6;
 
    // Retrieve rpc enabled TiVos
    $.getJSON("/getRpcTivos", function(data) {
@@ -17,64 +19,219 @@ $(document).ready(function() {
    .error(function(xhr, status) {
       handleError("/getRpcTivos", xhr, status);
    });
+
+   // NPL table
+   // NOTE: column 0 is a special column reserved for display
+   // of additional row information
+   $('#NPLTABLE').dataTable({
+     dom: 'T<"clear">lfrtip',
+     paging: false,
+     ordering: false, // turn off column sorting
+     autoWidth: true,
+     tableTools: {
+       "sRowSelect": "os",
+       "aButtons": [ "select_all", "select_none" ]
+     },
+     columns: [
+      {
+       "class":          'details-control',
+       "orderable":      false,
+       "data":           null,
+       "defaultContent": '',
+       "width":          "10px"
+      },
+      null,
+      { "width": "130px" },
+      { "width": "60px" },
+      { "width": "30px" },
+      { "width": "60px" },
+     ],
+   });
+
+   // FILE table
+   $('#FILETABLE').dataTable({
+     dom: 'T<"clear">lfrtip',
+     paging: false,
+     ordering: false, // turn off column sorting
+     autoWidth: true,
+     tableTools: {
+       "sRowSelect": "os",
+       "aButtons": [ "select_all", "select_none" ]
+     },
+     columns: [
+      null,
+     ],
+   });
+
+   // Add event listener for opening and closing row details
+   $('#NPLTABLE tbody').on('click', 'td.details-control', nplDetailsClicked);
 });
 
+
 function MyShows() {
+   var html = '<div style="color: blue">';
+   html += 'PLEASE WAIT: GETTING SHOWS FROM ' + TIVO.value + ' ...</div>';
+   BROWSE.innerHTML = html;
+   window.setInterval(function() {
+      $('#blinkText').toggle(); }, 300
+   );
+   clearNplTable();
+   hideTables();
+   showNplTable();
    var format = $('input[name="type"]:checked').val();
    var baseUrl = "/transcode?format=" + format + "&url=";
    $.getJSON("/getMyShows?tivo=" + encodeURIComponent(TIVO.value), function(data) {
-      var html = "";
-      $.each(data, function (i, entry) {
-         if (entry.hasOwnProperty("recording")) {
-            var json = entry.recording[0];
-            //console.log(JSON.stringify(json, null, 3));
-            html += '<a target="_blank" href="' + baseUrl;
-            if (json.hasOwnProperty("__url__")) {
-               url = json.__url__;
-               name = "";
-               if (json.hasOwnProperty("startTime")) {
-                  name = "(" + json.startTime + ") ";
-               }
-               if (json.hasOwnProperty("title")) {
-                  name += json.title;
-               }
-               if (json.hasOwnProperty("subtitle")) {
-                  name += " - " + json.subtitle;
-               }
-               html += encodeURIComponent(url) + '">' + name + '</a><br>';
-            }
-         }
-      });
-      BROWSE.innerHTML = html;
+      BROWSE.innerHTML = "";
+      loadNplData(data, baseUrl);
    })
    .error(function(xhr, status) {
+      BROWSE.innerHTML = "";
       handleError("/getMyShows", xhr, status);
    });
 }
 
+function loadNplData(data) {
+   $.each(data, function (i, entry) {
+      if (entry.hasOwnProperty("recording")) {
+         var json = entry.recording[0];
+         //console.log(JSON.stringify(json, null, 3));
+         if (json.hasOwnProperty("__url__")) {
+
+            var show = "";
+            if (json.hasOwnProperty("title")) {
+               show = json.title;
+            }
+            if (json.hasOwnProperty("subtitle")) {
+               show += " - " + json.subtitle;
+            }
+
+            var date = "";
+            if (json.hasOwnProperty("startTime")) {
+               date = getTime(json.startTime);
+            }
+
+            var channel = "";
+            if (json.hasOwnProperty("channel")) {
+               var chan = json.channel;
+               if (chan.hasOwnProperty("callSign")) {
+                  channel = chan.callSign;
+               } else {
+                  if (chan.has("name")) {
+                     channel = chan.name;
+                  }
+               }
+               if(chan.hasOwnProperty("channelNumber")) {
+                  channel += "=" + chan.channelNumber;
+               }
+            }
+
+            var dur = "";
+            if (json.hasOwnProperty("duration")) {
+               dur = secsToHM(json.duration);
+            }
+
+            var size = "";
+            if (json.hasOwnProperty("size")) {
+               var size_GB = json.size/Math.pow(2,20);
+               size = "%.2f GB".sprintf(size_GB);
+            }
+
+            // NOTE: 1st column is dummy used for hiding/unhiding row child info
+            // NOTE: Adding json data to end not associated with a table column
+            var row = $('#NPLTABLE').DataTable().row.add(
+               ["", show, date, channel, dur, size, json]
+            );
+            row.draw();
+         }
+      }
+   });
+}
+
+function secsToHM(secs) {
+   var hours = Math.floor(secs/3600);
+   var mins = Math.floor((secs - (hours * 3600))/60);
+   return "%d:%02d".sprintf(hours,mins);
+}
+
+function getTime(startTime) {
+   var week = ["Sat", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
+   var dt = new Date(Date.parse(startTime.replace(/-/g, "/") + " GMT"));
+   var date = "%s %02d/%02d/%02d %02d:%02d".sprintf(
+      week[dt.getUTCDay()],
+      dt.getMonth()+1,
+      dt.getDate(),
+      dt.getFullYear()-2000,
+      dt.getHours(),
+      dt.getMinutes()
+   );
+   return date;
+}
+
+function hideTables() {
+   NPLTABLE_DIV.style.display = 'none';
+   FILETABLE_DIV.style.display = 'none';
+}
+
+function clearNplTable() {
+   $('#NPLTABLE').DataTable().clear().draw();
+}
+
+function hideNplTable() {
+   NPLTABLE_DIV.style.display = 'none';
+}
+
+function showNplTable() {
+   NPLTABLE_DIV.style.display = 'block';
+}
+
+function clearFileTable() {
+   $('#FILETABLE').DataTable().clear().draw();
+}
+
+function hideFileTable() {
+   FILETABLE_DIV.style.display = 'none';
+}
+
+function showFileTable() {
+   FILETABLE_DIV.style.display = 'block';
+}
+
+function getUrl(json) {
+   var url = "";
+   if (json.hasOwnProperty("__url__"))
+      url = json.__url__;
+   return url;
+}
+
 function FileBrowser() {
+   clearFileTable();
+   hideTables();
+   showFileTable();
    var format = $('input[name="type"]:checked').val();
    var baseUrl = "/transcode?format=" + format + "&file=";
    $.getJSON("/getVideoFiles", function(data) {
-      var html = "";
-      $.each(data, function (i, file) {
-         html += '<a target="_blank" href="' + baseUrl;
-         html += encodeURIComponent(file) + '">' + file + '</a><br>';
-      });
-      BROWSE.innerHTML = html;
+      loadFileData(data);
    })
    .error(function(xhr, status) {
       handleError("/getVideoFiles", xhr, status);
    });
 }
 
+function loadFileData(data) {
+   $.each(data, function (i, file) {
+      if (file != "NONE") {
+         var row = $('#FILETABLE').DataTable().row.add([file]);
+         row.draw();
+      }
+   });
+}
+
 function GetCached() {
+   clearFileTable();
+   hideTables();
+   showFileTable();
    $.getJSON("/transcode?getCached=1", function(data) {
-      var html = "";
-      $.each(data, function (i, file) {
-         html += '<a target="_blank" href="' + file + '">' + file + '</a><br>';
-      });
-      BROWSE.innerHTML = html;
+      loadFileData(data);
    })
    .error(function(xhr, status) {
       handleError("/transcode?getCached", xhr, status);
@@ -107,6 +264,32 @@ function Running() {
    .error(function(xhr, status) {
       handleError("running", xhr, status);
    });
+}
+
+// Callback when details column is clicked on in a row
+// Hides or unhides row child data
+function nplDetailsClicked() {
+   var table = $('#NPLTABLE').DataTable();
+   var tr = $(this).closest('tr');
+   var row = table.row(tr);
+   console.log(row);
+
+   if ( row.child.isShown() ) {
+      // This row is already open - close it
+      row.child.hide();
+      tr.removeClass('shown');
+   }
+   else {
+      // Open this row
+      row.child( detailsFormat(row.data()) ).show();
+      tr.addClass('shown');
+   }
+}
+
+// This is formatting to use for the displaying row child data
+// Currently this displays whole json contents
+function detailsFormat(d) {
+   return '<pre>' + JSON.stringify(d[NUMCOLS], null, 3) + '</pre>';
 }
 
 function handleError(prefix, xhr, status) {
