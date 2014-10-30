024a8f3e3b85b965a6e35bfcbe08b0a1bf8c85c9
moyekj <moyekj@b0df3fdd-2057-4f73-b226-dfc9190e8f7a>
2014-10-30 22:55:08 +0000

    HLS related utils
    git-svn-id: https://svn.code.sf.net/p/kmttg/code/trunk@3142 b0df3fdd-2057-4f73-b226-dfc9190e8f7a

---
+++ src/com/tivo/kmttg/httpserver/Hlsutils.java
@@ -0,0 +1,60 @@
+package com.tivo.kmttg.httpserver;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
+
+import com.tivo.kmttg.util.file;
+import com.tivo.kmttg.util.log;
+
+public class Hlsutils {
+   private static String m3u8_terminator = "#EXT-X-ENDLIST";
+   
+   public static String getTextFileContents(String textFile) {
+      if ( ! file.isFile(textFile) )
+         return "";
+      String text = "";
+      try {
+         Scanner s = new Scanner(new File(textFile));
+         text = s.useDelimiter("\\A").next();
+         s.close();
+      } catch (Exception e) {}
+      return text;
+   }
+   
+   public static Boolean isPartial(String m3u8) {
+      Boolean partial = true;
+      String contents = getTextFileContents(m3u8);
+      if ( contents.contains(m3u8_terminator) )
+         partial = false;
+      return partial;
+   }
+   
+   public static void fixPartial(String m3u8) {
+      try {
+      BufferedWriter ofp = new BufferedWriter(new FileWriter(m3u8, true));
+      ofp.write(m3u8_terminator + "\r\n");
+      ofp.close();
+      } catch (Exception e) {
+         log.error("fixPartial - " + e.getMessage());
+      }
+   }
+   
+   // For .m3u8 file sum up the total time processed
+   // Simply sums all lines of following type: #EXTINF:12.078733,
+   public static float totalTime_m3u8(String fileName) {
+      float total = 0;
+      String text = getTextFileContents(fileName);
+      String[] lines = text.split("\n");
+      for (String line : lines) {
+         if (line.contains("#EXTINF")) {
+            line = line.replaceFirst("#EXTINF:", "");
+            line = line.replace(",", "");
+            total += Float.parseFloat(line);
+         }
+      }
+      return total;
+   }
+
+}
