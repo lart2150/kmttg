686f6aa6a6c367a20c1b5c12fbdc9b4bf5a2251c
moyekj <moyekj@b0df3fdd-2057-4f73-b226-dfc9190e8f7a>
2015-09-05 23:36:42 +0000

    Don't search for partners outside of bodyId
    git-svn-id: https://svn.code.sf.net/p/kmttg/code/trunk@3585 b0df3fdd-2057-4f73-b226-dfc9190e8f7a

--- src/com/tivo/kmttg/gui/table/TableUtil.java
+++ src/com/tivo/kmttg/gui/table/TableUtil.java
@@ -1127,7 +1127,7 @@ public class TableUtil {
          String name = partnerId;
          if (config.partners.containsKey(partnerId))
             name = config.partners.get(partnerId);
-         if (name.equals(partnerId) && config.getTivoUsername() != null) {
+         /*if (name.equals(partnerId) && config.getTivoUsername() != null) {
             // Not cached, so find it without bodyId from middlemind if tivo.com credentials available
             Remote r = new Remote(config.gui.remote_gui.getTivoName("search"), true);
             if (r.success) {
@@ -1143,7 +1143,7 @@ public class TableUtil {
                   }
                }
             }
-         }
+         }*/
          return name;
       } catch (JSONException e1) {
          log.error("getPartnerName - " + e1.getMessage());
