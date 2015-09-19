889515cabb0b9ecf1e80dfe7c0d06252d5d65c5d
moyekj <moyekj@b0df3fdd-2057-4f73-b226-dfc9190e8f7a>
2015-09-19 17:18:58 +0000

    Added .ts extension handling
    git-svn-id: https://svn.code.sf.net/p/kmttg/code/trunk@3630 b0df3fdd-2057-4f73-b226-dfc9190e8f7a

--- src/com/tivo/kmttg/task/tivolibre.java
+++ src/com/tivo/kmttg/task/tivolibre.java
@@ -6,6 +6,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.Serializable;
 import java.util.Date;
+import java.util.Hashtable;
 
 import com.tivo.kmttg.main.config;
 import com.tivo.kmttg.main.jobData;
@@ -13,8 +14,11 @@ import com.tivo.kmttg.main.jobMonitor;
 import com.tivo.kmttg.rpc.rnpl;
 import com.tivo.kmttg.util.backgroundProcess;
 import com.tivo.kmttg.util.debug;
+import com.tivo.kmttg.util.ffmpeg;
 import com.tivo.kmttg.util.file;
 import com.tivo.kmttg.util.log;
+import com.tivo.kmttg.util.mediainfo;
+import com.tivo.kmttg.util.string;
 
 import net.straylightlabs.tivolibre.TivoDecoder;
 
@@ -69,7 +73,56 @@ public class tivolibre extends baseTask implements Serializable {
 
    public Boolean start() {
       debug.print("");
+      // Obtain some info on input video
+      Hashtable<String,String> info = null;
+      if (file.isFile(config.mediainfo))
+         info = mediainfo.getVideoInfo(job.tivoFile);
+      else if (file.isFile(config.ffmpeg))
+         info = ffmpeg.getVideoInfo(job.tivoFile);
+
+      // For transport stream container input files change output file suffix from .mpg to .ts
+      Boolean isFileChanged = false;
+      if (info != null && info.get("container").equals("mpegts")) {
+         if (job.mpegFile.endsWith(".mpg")) {
+            job.mpegFile = string.replaceSuffix(job.mpegFile, ".ts");
+            isFileChanged = true;
+         }
+      }      
+      if (isFileChanged) {            
+         // If in GUI mode, update job monitor output field
+         if (config.GUIMODE) {
+            String output = string.basename(job.mpegFile);
+            if (config.jobMonitorFullPaths == 1)
+               output = job.mpegFile;
+            config.gui.jobTab_UpdateJobMonitorRowOutput(job, output);
+         }
+         
+         // Subsequent jobs need to have mpegFile && mpegFile_cut updated
+         jobMonitor.updatePendingJobFieldValue(job, "mpegFile", job.mpegFile);
+         String mpegFile_cut = job.mpegFile_cut.replaceFirst("_cut.mpg", "_cut.ts");
+         jobMonitor.updatePendingJobFieldValue(job, "mpegFile_cut", mpegFile_cut);
+         
+         // Rename already created metadata file if relevant
+         String meta = string.replaceSuffix(job.mpegFile, ".mpg") + ".txt";
+         if (file.isFile(meta)) {
+            String meta_new = job.mpegFile + ".txt";
+            log.print("Renaming metadata file to: " + meta_new);
+            file.rename(meta, meta_new);
+            // Subsequent jobs need to have metaFile updated
+            jobMonitor.updatePendingJobFieldValue(job, "metaFile", meta_new);
+         }
+         meta = string.replaceSuffix(job.mpegFile_cut, ".mpg") + ".txt";
+         if (file.isFile(meta)) {
+            String meta_new = job.mpegFile_cut + ".txt";
+            log.print("Renaming metadata file to: " + meta_new);
+            file.rename(meta, meta_new);
+            // Subsequent jobs need to have metaFile updated
+            jobMonitor.updatePendingJobFieldValue(job, "metaFile", meta_new);
+         }
+      }
+
       // Run download method in a separate thread
+      log.print(">> DECRYPTING USING TIVOLIBRE " + job.tivoFile + " ...");
       Runnable r = new Runnable() {
          public void run () {
             try {
@@ -136,7 +189,7 @@ public class tivolibre extends baseTask implements Serializable {
          jobMonitor.removeFromJobList(job);
          
          if ( success ) {
-            log.warn("tibolibre job completed: " + jobMonitor.getElapsedTime(job.time));
+            log.warn("tivolibre job completed: " + jobMonitor.getElapsedTime(job.time));
             log.print("---DONE--- job=" + job.type + " output=" + job.mpegFile);
             
             // Remove .TiVo file if option enabled
