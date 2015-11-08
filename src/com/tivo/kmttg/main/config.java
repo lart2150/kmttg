6bb069c0e6ad919e11e017b482b3e44af409b6e1
moyekj <moyekj@b0df3fdd-2057-4f73-b226-dfc9190e8f7a>
2015-11-08 15:54:27 +0000

    Version update
    git-svn-id: https://svn.code.sf.net/p/kmttg/code/trunk@3787 b0df3fdd-2057-4f73-b226-dfc9190e8f7a

--- src/com/tivo/kmttg/main/config.java
+++ src/com/tivo/kmttg/main/config.java
@@ -23,7 +23,7 @@ import com.tivo.kmttg.gui.gui;
 import com.tivo.kmttg.httpserver.kmttgServer;
 
 public class config {
-   public static String kmttg = "kmttg v2.0u";
+   public static String kmttg = "kmttg v2.0v";
    
    // encoding related
    public static String encProfDir = "";
