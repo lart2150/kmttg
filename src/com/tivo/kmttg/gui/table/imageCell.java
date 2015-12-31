e4463fb9f155bf54a2da88a9a99728965a9591dc
moyekj <moyekj@b0df3fdd-2057-4f73-b226-dfc9190e8f7a>
2015-12-31 15:39:27 +0000

    Added 2nd image
    git-svn-id: https://svn.code.sf.net/p/kmttg/code/trunk@3839 b0df3fdd-2057-4f73-b226-dfc9190e8f7a

--- src/com/tivo/kmttg/gui/table/imageCell.java
+++ src/com/tivo/kmttg/gui/table/imageCell.java
@@ -7,17 +7,22 @@ import javafx.scene.layout.HBox;
 
 public class imageCell extends HBox {
    private ImageView image = new ImageView();
+   private ImageView image2 = new ImageView();
    private Label label = new Label("");
    public String imageName = "";
    
    public imageCell() {
       super();
-      setSpacing(3);
-      getChildren().addAll(image, label);
+      setSpacing(1);
+      getChildren().addAll(image, image2, label);
    }
    
    public void setImage(Image img) {
       image.setImage(img);
+   }
+   
+   public void setImage2(Image img) {
+      image2.setImage(img);
    }
    
    public void setLabel(String s) {
