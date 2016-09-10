1c9fcd467c35d801cd02ef326549b34f0805e58c
moyekj <moyekj@b0df3fdd-2057-4f73-b226-dfc9190e8f7a>
2016-09-10 05:46:20 +0000

    Added printIds method
    git-svn-id: https://svn.code.sf.net/p/kmttg/code/trunk@4064 b0df3fdd-2057-4f73-b226-dfc9190e8f7a

--- src/com/tivo/kmttg/rpc/id.java
+++ src/com/tivo/kmttg/rpc/id.java
@@ -5,6 +5,17 @@ import com.tivo.kmttg.util.log;
 
 public class id {
    
+   public static void printIds(JSONObject json) {
+      String p = programId(json);
+      String s = seriesId(json);
+      if (p != null || s != null)
+         log.print("pyTivo metadata id fields:");
+      if (p != null)
+         log.print("programdId : " + p);
+      if (s != null)
+         log.print("seriesId : " + s);
+   }
+   
    // Return a Rovi style programId based on json containing collectionId & contentId
    // "collectionId":"tivo:cl.328422972" "contentId": "tivo:ct.328421836" => EP0328422972-0328421836
    public static String programId(JSONObject json) {
