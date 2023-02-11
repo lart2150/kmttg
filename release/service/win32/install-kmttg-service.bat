4dd5c0141719102900790ab9f2e81bb9798c5a00
Brian Engert <lart2150@gmail.com>
2023-02-11 17:02:09 -0600

    Revert "feat: switch service to apache procrun to fix x86_64 jra compataiblity with the free wrapper.exe"
    This reverts commit f5b9ce71210440134d32c5e73a88122b3843b3f9.

--- release/service/win32/install-kmttg-service.bat
+++ release/service/win32/install-kmttg-service.bat
@@ -1,12 +1,57 @@
 @echo off
 setlocal
 
-set SERVICE_PATH=%~dp0
-cd ..\..
+rem Copyright (c) 1999, 2008 Tanuki Software, Inc.
+rem http://www.tanukisoftware.com
+rem All rights reserved.
+rem
+rem This software is the proprietary information of Tanuki Software.
+rem You shall use it only in accordance with the terms of the
+rem license agreement you entered into with Tanuki Software.
+rem http://wrapper.tanukisoftware.org/doc/english/licenseOverview.html
+rem
+rem Java Service Wrapper general NT service install script.
+rem Optimized for use with version 3.3.1 of the Wrapper.
+rem
 
-if %OS%==64BIT (
-	%SERVICE_PATH%apache\amd64\prunsrv //IS//kmttg --DisplayName="kmttg" --Startup=auto --Install=%SERVICE_PATH%apache\amd64\prunsrv.exe --Jvm=auto --StopTimeout=10 --LogPath=%SERVICE_PATH% --StdOutput=auto --StdError=auto --StartMode=java --Classpath=%%CLASSPATH%%;%cd%\kmttg.jar  --StartClass=com.tivo.kmttg.main.kmttg --StartParams=-a
-) else (
-	%SERVICE_PATH%apache\prunsrv //IS//kmttg --DisplayName="kmttg" --Startup=auto --Install=%SERVICE_PATH%apache\prunsrv.exe             --Jvm=auto --StopTimeout=10 --LogPath=%SERVICE_PATH% --StdOutput=auto --StdError=auto --StartMode=java --Classpath=%%CLASSPATH%%;%cd%\kmttg.jar  --StartClass=com.tivo.kmttg.main.kmttg --StartParams=-a
-)
-cd %SERVICE_PATH%
+if "%OS%"=="Windows_NT" goto nt
+echo This script only works with NT-based versions of Windows.
+goto :eof
+
+:nt
+rem
+rem Find the application home.
+rem
+rem %~dp0 is location of current script under NT
+set _REALPATH=%~dp0
+rem Decide on the wrapper binary.
+set _WRAPPER_BASE=bin/wrapper
+set _WRAPPER_EXE=%_REALPATH%%_WRAPPER_BASE%-windows-x86-32.exe
+if exist "%_WRAPPER_EXE%" goto conf
+set _WRAPPER_EXE=%_REALPATH%%_WRAPPER_BASE%-windows-x86-64.exe
+if exist "%_WRAPPER_EXE%" goto conf
+set _WRAPPER_EXE=%_REALPATH%%_WRAPPER_BASE%.exe
+if exist "%_WRAPPER_EXE%" goto conf
+echo Unable to locate a Wrapper executable using any of the following names:
+echo %_REALPATH%%_WRAPPER_BASE%-windows-x86-32.exe
+echo %_REALPATH%%_WRAPPER_BASE%-windows-x86-64.exe
+echo %_REALPATH%%_WRAPPER_BASE%.exe
+pause
+goto :eof
+
+rem
+rem Find the wrapper.conf
+rem
+:conf
+set _WRAPPER_CONF="%~f1"
+if not %_WRAPPER_CONF%=="" goto startup
+set _WRAPPER_CONF="%_REALPATH%..\conf\wrapper.conf"
+
+rem
+rem Install the Wrapper as an NT service.
+rem
+:startup
+"%_WRAPPER_EXE%" -i %_WRAPPER_CONF%
+if not errorlevel 1 goto :eof
+pause
+
