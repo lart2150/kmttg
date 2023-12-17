06613aafa75b045e28efd4fcc914598924d13a19
Brian Engert <lart2150@gmail.com>
2023-12-17 11:09:28 -0600

    feat: bumping version to 2.8

--- src/com/tivo/kmttg/main/config.java
+++ src/com/tivo/kmttg/main/config.java
@@ -41,7 +41,7 @@ import com.tivo.kmttg.gui.gui;
 import com.tivo.kmttg.httpserver.kmttgServer;
 
 public class config {
-   public static String kmttg = "kmttg v2.8-l-pre";
+   public static String kmttg = "kmttg v2.8-l";
    
    // encoding related
    public static String encProfDir = "";
