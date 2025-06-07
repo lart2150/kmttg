35fc27c1815310048180d893436556303a65f930
Brian Engert <lart2150@gmail.com>
2025-06-07 17:31:48 -0500

    fix: update kmttg.ps1 incase there is more then one javafx folder

--- release/kmttg.ps1
+++ release/kmttg.ps1
@@ -1,9 +1,9 @@
 
 $kttgDir = Split-Path $MyInvocation.MyCommand.Path -Parent
-$javaFXweb = Get-Childitem –Path $kttgDir -Include "javafx.web.jar" -Recurse -ErrorAction SilentlyContinue
+$javaFXweb = Get-Childitem –Path $kttgDir -Include "javafx.web.jar" -Recurse -ErrorAction SilentlyContinue | Select-Object -First 1
 
 if ($javaFXweb) {
-    $javaFX = Split-Path $javaFXweb -Parent;
+    $javaFX = $javaFXweb.DirectoryName;
     java --module-path "$javaFX" --add-modules javafx.web -jar "$kttgDir\kmttg.jar" $args
 } else {
     java -jar "$kttgDir\kmttg.jar" $args
