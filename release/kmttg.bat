bf3c7ae4a134f72397263e58f8f7f33e45335b67
Brian Engert <lart2150@gmail.com>
2026-06-18 08:23:33 -0500

    feat: add support for kmttg.vmoptions file to hold jvm params

--- release/kmttg.bat
+++ release/kmttg.bat
@@ -1,5 +1,14 @@
 @echo off
+setlocal enabledelayedexpansion
 SET KMTTG_BAT=%~dp0
 SET KMTTG_DIR=%KMTTG_BAT:~0,-1%
 
-java -jar "%KMTTG_BAT%\kmttg.jar" %*
+REM Read optional JVM parameters, one per line, from kmttg.vmoptions
+SET VMOPTIONS=
+IF EXIST "%KMTTG_BAT%kmttg.vmoptions" (
+    FOR /F "usebackq eol=# delims=" %%a IN ("%KMTTG_BAT%kmttg.vmoptions") DO (
+        SET VMOPTIONS=!VMOPTIONS! %%a
+    )
+)
+
+java !VMOPTIONS! -jar "%KMTTG_BAT%\kmttg.jar" %*
--- release/kmttg.ps1
+++ release/kmttg.ps1
@@ -1,4 +1,11 @@
 
 $kttgDir = Split-Path $MyInvocation.MyCommand.Path -Parent
 
-java -jar "$kttgDir\kmttg.jar" $args
+# Read optional JVM parameters, one per line, from kmttg.vmoptions
+$vmOptions = @()
+$vmOptionsFile = Join-Path $kttgDir "kmttg.vmoptions"
+if (Test-Path $vmOptionsFile) {
+    $vmOptions = Get-Content $vmOptionsFile | Where-Object { $_.Trim() -ne "" -and -not $_.Trim().StartsWith("#") }
+}
+
+java @vmOptions -jar "$kttgDir\kmttg.jar" $args
