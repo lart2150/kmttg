182f0de9b851481240cfcae73b807752bb0d192b
Brian Engert <lart2150@gmail.com>
2026-09-07 11:11:22 -0500

    chore: bump version to v2.11.0-rc1

--- src/com/tivo/kmttg/main/config.java
+++ src/com/tivo/kmttg/main/config.java
@@ -42,7 +42,7 @@ import com.tivo.kmttg.gui.gui;
 import com.tivo.kmttg.httpserver.kmttgServer;
 
 public class config {
-   public static String kmttg = "kmttg v2.10.0-l";
+   public static String kmttg = "kmttg v2.11.0-rc1";
    
    // encoding related
    public static String encProfDir = "";
