b91694fdaeab58665dedd73b8029ead4f555519f
Brian Engert <lart2150@gmail.com>
2026-09-19 18:04:13 -0500

    build: fix version number

--- src/com/tivo/kmttg/main/config.java
+++ src/com/tivo/kmttg/main/config.java
@@ -42,7 +42,7 @@ import com.tivo.kmttg.gui.gui;
 import com.tivo.kmttg.httpserver.kmttgServer;
 
 public class config {
-   public static String kmttg = "kmttg v2.11.0";
+   public static String kmttg = "kmttg v2.11.0-l";
    
    // encoding related
    public static String encProfDir = "";
